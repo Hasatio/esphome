df677ff039ff80de80ec6b2201233ae8b795c7b6
wert95 <wert.95@hotmail.com>
2023-03-30 06:52:40 +0300

    Update __init__.py

--- components/pca9685custom/__init__.py
+++ components/pca9685custom/__init__.py
@@ -3,8 +3,13 @@ import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_FREQUENCY, CONF_ID, CONF_EXTERNAL_CLOCK_INPUT
 
+from esphome import automation
+from esphome.automation import maybe_simple_id
+
 DEPENDENCIES = ["i2c"]
 MULTI_CONF = True
+
+CONF_VOLUME = "volume"
 
 pca9685custom_ns = cg.esphome_ns.namespace("pca9685custom")
 PCA9685customOutput = pca9685custom_ns.class_("PCA9685customOutput", cg.Component, i2c.I2CDevice)
@@ -45,3 +50,27 @@ async def to_code(config):
     cg.add(var.set_extclk(config[CONF_EXTERNAL_CLOCK_INPUT]))
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
+
+PCA9685customDoseVolumeAction = pca9685_ns.class_("PCA9685customDoseVolumeAction", automation.Action)
+
+PCA9685custom_DOSE_VOLUME_ACTION_SCHEMA = cv.All(
+    {
+        cv.Required(CONF_ID): cv.use_id(PCA9685custom),
+        cv.Required(CONF_VOLUME): cv.templatable(
+            cv.float_range()
+        ),  # Any way to represent as proper volume (vs. raw int)
+    }
+)
+
+
+@automation.register_action(
+    "pca9685.dose_volume", PCA9685customDoseVolumeAction, PCA9685custom_DOSE_VOLUME_ACTION_SCHEMA
+)
+async def pca9685_dose_volume_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+
+    template_ = await cg.templatable(config[CONF_VOLUME], args, cg.double)
+    cg.add(var.set_volume(template_))
+
+    return var
