7f2065a9b65afca7c783fa2090dce418f2a825c3
wert95 <wert.95@hotmail.com>
2023-07-04 16:54:13 +0300

    .

--- components/custom/__init__.py
+++ components/custom/__init__.py
@@ -8,13 +8,14 @@ DEPENDENCIES = ['i2c']
 custom_ns = cg.esphome_ns.namespace('custom')
 Custom = custom_ns.class_('Custom', output.FloatOutput, cg.Component)
 
-CONFIG_SCHEMA = cv.Schema(
+CONFIG_SCHEMA = cv.ALL(
+cv.Schema(
 {
-    cv.GenerateID(): cv.declare_id(Custom),
+    cv.GenerateID(): cv.declare_id(Custom)
 }
 )
 .extend(cv.COMPONENT_SCHEMA)
-
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
