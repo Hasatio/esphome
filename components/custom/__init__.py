2d4177e7ef39cf990d290d25f94fddb313836c2f
wert95 <wert.95@hotmail.com>
2023-07-04 17:09:44 +0300

    .

--- components/custom/__init__.py
+++ components/custom/__init__.py
@@ -8,12 +8,9 @@ DEPENDENCIES = ['i2c']
 custom_ns = cg.esphome_ns.namespace('custom')
 Custom = custom_ns.class_('Custom', output.FloatOutput, cg.Component)
 
-CONFIG_SCHEMA = cv.Schema(
-{
+CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Custom),
-}
-)
-.extend(cv.COMPONENT_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
--- components/custom/output.py
+++ components/custom/output.py
@@ -4,13 +4,9 @@ from esphome.components import output
 from esphome.const import CONF_ID
 from . import custom, custom_ns
 
-CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend
-(
-{
+CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({
     cv.Required(CONF_ID): cv.declare_id(Custom),
-}
-)
-.extend(cv.COMPONENT_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
