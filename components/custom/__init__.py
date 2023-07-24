5a5c0d3edaaa9381fe40c15cbfcbf0566627a28a
wert95 <wert.95@hotmail.com>
2023-07-24 19:41:37 +0300

    Update __init__.py

--- components/custom/__init__.py
+++ components/custom/__init__.py
@@ -9,18 +9,19 @@ CONF_VARIABLES
 )
 
 #DEPENDENCIES = ["i2c"]
+CONF_ON_CUSTOM = "on_custom"
 
 custom_ns = cg.esphome_ns.namespace("custom")
 Custom = custom_ns.class_("Custom", output.FloatOutput, cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Custom),
-    cv.Optional(CONF_VARIABLES): cv.float_
+    cv.Optional(CONF_ON_CUSTOM): cv.float_
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield output.register_output(var, config)
-    cg.add(var.set_variables(config[CONF_VARIABLES]))
+    cg.add(var.set_variables(config[CONF_ON_CUSTOM]))
     
