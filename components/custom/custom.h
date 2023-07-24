7af1a5c8c7f96e738db9ce63a6b3c1e3ee25f56f
wert95 <wert.95@hotmail.com>
2023-07-24 16:31:50 +0300

    Update custom.h

--- components/custom/custom.h
+++ components/custom/custom.h
@@ -20,10 +20,10 @@ class Custom : public output::FloatOutput, public Component, public i2c::I2CDevi
   void dump_config() override;
 
 
-  void set_baud_rate(uint32_t baud_rate) { baud_rate_ = baud_rate; }
-  uint32_t get_baud_rate() const { return baud_rate_; }
+  // void set_baud_rate(uint32_t baud_rate) { baud_rate_ = baud_rate; }
+  // uint32_t get_baud_rate() const { return baud_rate_; }
 
-  void check_uart_settings(uint32_t baud_rate);
+  // void check_uart_settings(uint32_t baud_rate);
 
 protected:
   uint32_t baud_rate_;
