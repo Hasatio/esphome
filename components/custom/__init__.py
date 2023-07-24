077a6e8571631cd5e6f070e5ecb2ffb5a11ba8ef
wert95 <wert.95@hotmail.com>
2023-07-24 16:48:43 +0300

    Update __init__.py

--- components/custom/__init__.py
+++ components/custom/__init__.py
@@ -17,7 +17,7 @@ Custom = custom_ns.class_("Custom", output.FloatOutput, cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Custom),
     cv.Optional(CONF_BAUD_RATE): cv.int_range(min=1),
-    cv.Optional(CONF_FLOAT): cv.int_range(0.0)
+    cv.Optional(CONF_FLOAT): cv.int_range(min=0.0)
 }).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -25,3 +25,4 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     cg.add(var.set_baud_rate(config[CONF_BAUD_RATE]))
+    cg.add(var.set_float(config[CONF_FLOAT]))
