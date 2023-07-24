9d2de75e434b6e57043dc34bdc8084a417571ff3
wert95 <wert.95@hotmail.com>
2023-07-24 15:40:23 +0300

    Update __init__.py

--- components/empty_component/__init__.py
+++ components/empty_component/__init__.py
@@ -6,16 +6,8 @@ from esphome.const import CONF_ID
 empty_component_ns = cg.esphome_ns.namespace('empty_component')
 EmptyComponent = empty_component_ns.class_('EmptyComponent', output.FloatOutput, cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(EmptyComponent)
-}).extend(cv.COMPONENT_SCHEMA)
-CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({
-    cv.Required(CONF_ID): cv.declare_id(EmptyFloatOutput),
-}).extend(cv.COMPONENT_SCHEMA)
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+CONFIG_SCHEMA = cv.Schema({cv.GenerateID(): cv.declare_id(EmptyComponent)}).extend(cv.COMPONENT_SCHEMA)
+# CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({cv.Required(CONF_ID): cv.declare_id(EmptyComponent)}).extend(cv.COMPONENT_SCHEMA)
     
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
