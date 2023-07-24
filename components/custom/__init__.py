350d7057261b77b86ca4f19634c9fdb7a51db99c
wert95 <wert.95@hotmail.com>
2023-07-24 17:26:32 +0300

    Update __init__.py

--- components/custom/__init__.py
+++ components/custom/__init__.py
@@ -20,9 +20,10 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_VARIABLES): cv.float_
 }).extend(cv.COMPONENT_SCHEMA)
 
-
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
+    yield output.register_output(var, config)
     cg.add(var.set_baud_rate(config[CONF_BAUD_RATE]))
     cg.add(var.set_variables(config[CONF_VARIABLES]))
+    
