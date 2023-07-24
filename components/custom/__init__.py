ece94ab41300fbd7742fcb89e01ec8364797c1ac
wert95 <wert.95@hotmail.com>
2023-07-24 17:09:10 +0300

    Update __init__.py

--- components/custom/__init__.py
+++ components/custom/__init__.py
@@ -6,7 +6,7 @@ from esphome.components import i2c, sensor, output
 from esphome.const import (
 CONF_ID,
 CONF_BAUD_RATE,
-CONF_FLOAT,
+CONF_VARIABLES,
 )
 
 #DEPENDENCIES = ["i2c"]
@@ -17,7 +17,7 @@ Custom = custom_ns.class_("Custom", output.FloatOutput, cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Custom),
     cv.Optional(CONF_BAUD_RATE): cv.int_range(min=1),
-    cv.Optional(CONF_FLOAT): cv.int_range(min=0.0)
+    cv.Optional(CONF_VARIABLES): cv.int_range(min=0.0)
 }).extend(cv.COMPONENT_SCHEMA)
 
 
@@ -25,4 +25,4 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     cg.add(var.set_baud_rate(config[CONF_BAUD_RATE]))
-    cg.add(var.set_float(config[CONF_FLOAT]))
+    cg.add(var.set_variables(config[CONF_VARIABLES]))
