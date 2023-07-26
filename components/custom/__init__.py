1d3c927d225343d634f9f8c88b23accc24f1e496
wert95 <wert.95@hotmail.com>
2023-07-26 21:44:10 +0300

    Update __init__.py

--- components/custom/__init__.py
+++ components/custom/__init__.py
@@ -26,28 +26,28 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await output.register_output(var, config)
-    cg.add(var.set_variables(config[CONF_ON_CUSTOM]))
+    # cg.add(var.set_variables(config[CONF_ON_CUSTOM]))
     
-# Custom_action = custom_ns.class_("Custom_action", automation.Action)
+Custom_action = custom_ns.class_("Custom_action", automation.Action)
 
-# CUSTOM_ACTION_SCHEMA = maybe_simple_id(
-#     {
-#         cv.Required(CONF_ID): cv.use_id(Custom),
-#         cv.Optional(CONF_ON_CUSTOM): cv.templatable(cv.float_range()),
-#     }
-# )
+CUSTOM_ACTION_SCHEMA = maybe_simple_id(
+    {
+        cv.Required(CONF_ID): cv.use_id(Custom),
+        cv.Optional(CONF_ON_CUSTOM): cv.templatable(cv.float_range()),
+    }
+)
     
-# @automation.register_action(
-#     "custom.set_variables",
-#     Custom_action,
-#     CUSTOM_ACTION_SCHEMA,
-# )
+@automation.register_action(
+    "custom.set_variables",
+    Custom_action,
+    CUSTOM_ACTION_SCHEMA,
+)
     
-# async def custom_to_code(config, action_id, template_arg, args):
-#     paren = await cg.get_variable(config[CONF_ID])
-#     var = cg.new_Pvariable(action_id, template_arg, paren)
+async def custom_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
 
-#     template_ = await cg.templatable(config[CONF_ON_CUSTOM], args, cg.double)
-#     cg.add(var.set_variables(template_))
+    template_ = await cg.templatable(config[CONF_ON_CUSTOM], args, cg.double)
+    cg.add(var.set_variables(template_))
 
-#     return var
+    return var
