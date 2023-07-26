a902a04a69d64cdf11d402a9783ac85d6677f855
wert95 <wert.95@hotmail.com>
2023-07-26 07:37:59 +0300

    Update __init__.py

--- components/custom/__init__.py
+++ components/custom/__init__.py
@@ -3,10 +3,7 @@ from typing import Optional
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor, output
-from esphome.const import (
-CONF_ID,
-CONF_VARIABLES
-)
+from esphome.const import (CONF_ID)
 from esphome import automation
 from esphome.automation import maybe_simple_id
 
