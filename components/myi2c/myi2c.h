a9b3989c4aea0a7b2eea4cbbe55a9c8b3191655d
wert95 <wert.95@hotmail.com>
2023-07-27 22:30:38 +0300

    Update myi2c.h

--- components/myi2c/myi2c.h
+++ components/myi2c/myi2c.h
@@ -8,13 +8,6 @@
 #include <Adafruit_MAX1704X.h>
 #include <BluetoothSerial.h>
 
-namespace esphome {
-namespace myi2c {
-
-class Myi2c : public Component, public Sensor 
-{
-public:
-
 BluetoothSerial SerialBT;
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -59,6 +52,13 @@ Adafruit_MAX17048 maxlipo;
 
 TwoWire I2C_1 = TwoWire(0);
 //TwoWire I2C_2 = TwoWire(1);
+
+namespace esphome {
+namespace myi2c {
+
+class Myi2c : public Component, public Sensor 
+{
+public:
 
    float get_setup_priority() const override { return esphome::setup_priority::PROCESSOR; }
 
