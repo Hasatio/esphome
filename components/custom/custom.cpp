37ba7d5e37430814d1a8615c8aeb5267b55a2bdf
wert95 <wert.95@hotmail.com>
2023-07-27 18:50:39 +0300

    Update custom.cpp

--- components/custom/custom.cpp
+++ components/custom/custom.cpp
@@ -8,26 +8,11 @@ static const char *TAG = "custom";
 double vr;
 void Custom::setup(){
 
-    pinMode(13, OUTPUT);
-    
-    ESP_LOGD(TAG, "setup");
 }
 
 void Custom::loop(){
 
     ESP_LOGD(TAG, "loop %f",vr);
-}
-
-void Custom::write_state(float state){
-
-    ESP_LOGD(TAG, "Empty custom float output %f",state);
-    int value = state * 1024;
-    analogWrite(13, value);
-    this->set_level(state != this->inverted_ ? 1.0f : 0.0f);
-}
-
-void Custom::dump_config() {
-
 }
 
 void Custom::set_variables(double var){
