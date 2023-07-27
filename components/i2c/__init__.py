689dbea9c49f3469ed5931d75df85c41cabb5e9c
wert95 <wert.95@hotmail.com>
2023-07-27 20:21:41 +0300

    Create __init__.py

---
+++ components/i2c/__init__.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor, output
+from esphome.const import (CONF_ID)
+
+#DEPENDENCIES = ["i2c"]
+CONF_MY_OUTPUT = "output"
+
+i2c_ns = cg.esphome_ns.namespace("myi2c")
+Myi2cComponent = i2c_ns.class_("Myi2cComponent", cg.Component)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+        cv.GenerateID(): cv.declare_id(Custom),
+        cv.Optional(CONF_MY_OUTPUT): cv.float_range()
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    cg.add(var.gain(config[CONF_MY_OUTPUT]))
