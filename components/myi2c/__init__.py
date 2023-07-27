f7bd040be9b22ac47f65cfc9f192c81e9181148d
wert95 <wert.95@hotmail.com>
2023-07-28 02:29:06 +0300

    Update __init__.py

--- components/myi2c/__init__.py
+++ components/myi2c/__init__.py
@@ -6,8 +6,8 @@ from esphome.components import i2c, sensor, output
 from esphome.const import (CONF_ID)
 
 #DEPENDENCIES = ["i2c"]
-CONF_MY_GAIN = "gain"
-CONF_MY_GSIN = "hebele"
+CONF_MY_GAIN = "gain",
+CONF_MY_BLUETOOTH = "bluetooth"
 
 i2c_ns = cg.esphome_ns.namespace("myi2c") # esphome component adı
 Myi2c = i2c_ns.class_("Myi2c", cg.Component)
@@ -17,7 +17,7 @@ CONFIG_SCHEMA = (
         {
         cv.GenerateID(): cv.declare_id(Myi2c),
         cv.Optional(CONF_MY_GAIN): cv.float_,
-        cv.Optional(CONF_MY_GSIN): cv.string,
+        cv.Optional(CONF_MY_BLUETOOTH): cv.string,
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -27,4 +27,4 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     cg.add(var.gain(config[CONF_MY_GAIN]))
-    cg.add(var.bluetooth(config[CONF_MY_GSIN]))
+    cg.add(var.bluetooth(config[CONF_MY_BLUETOOTH]))
