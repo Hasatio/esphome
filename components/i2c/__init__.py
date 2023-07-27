06019ea0325cc82d63cce0faa70259cba08b0dcf
wert95 <wert.95@hotmail.com>
2023-07-27 20:31:27 +0300

    Update __init__.py

--- components/i2c/__init__.py
+++ components/i2c/__init__.py
@@ -7,6 +7,7 @@ from esphome.const import (CONF_ID)
 
 #DEPENDENCIES = ["i2c"]
 CONF_MY_OUTPUT = "output"
+CONF_MY_BLUETOOTH = "bluetooth"
 
 i2c_ns = cg.esphome_ns.namespace("myi2c")
 Myi2cComponent = i2c_ns.class_("Myi2cComponent", cg.Component)
@@ -15,7 +16,8 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
         cv.GenerateID(): cv.declare_id(Custom),
-        cv.Optional(CONF_MY_OUTPUT): cv.float_range()
+        cv.Optional(CONF_MY_OUTPUT): cv.float_
+        cv.Optional(CONF_MY_BLUETOOTH): cv.std_string
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -25,3 +27,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     cg.add(var.gain(config[CONF_MY_OUTPUT]))
+    cg.add(var.device(config[CONF_MY_BLUETOOTH]))
