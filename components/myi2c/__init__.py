421652a797549319c6022add0db5312e88ac66ba
wert95 <wert.95@hotmail.com>
2023-07-28 00:59:15 +0300

    Update __init__.py

--- components/myi2c/__init__.py
+++ components/myi2c/__init__.py
@@ -7,7 +7,7 @@ from esphome.const import (CONF_ID)
 
 #DEPENDENCIES = ["i2c"]
 CONF_MY_GAIN = "gain"
-CONF_MY_BT_NAME = "btname"
+CONF_MY_BT_NAME = "bt_name"
 
 i2c_ns = cg.esphome_ns.namespace("myi2c") # esphome component adı
 Myi2c = i2c_ns.class_("Myi2c", cg.Component)
