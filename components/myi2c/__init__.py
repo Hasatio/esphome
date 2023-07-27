bc03e31842d8a42985c883824212e3fab68b4b60
wert95 <wert.95@hotmail.com>
2023-07-28 00:57:35 +0300

    Update __init__.py

--- components/myi2c/__init__.py
+++ components/myi2c/__init__.py
@@ -17,7 +17,7 @@ CONFIG_SCHEMA = (
         {
         cv.GenerateID(): cv.declare_id(Myi2c),
         cv.Optional(CONF_MY_GAIN): cv.float_,
-        cv.Optional(CONF_MY_BT): cv.std_string,
+        cv.Optional(CONF_MY_BT): cv.string,
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
