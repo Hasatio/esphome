a4389ee90dc653835bfc3e94847e377db53bf532
wert95 <wert.95@hotmail.com>
2023-07-28 03:12:17 +0300

    Update __init__.py

--- components/myi2c/__init__.py
+++ components/myi2c/__init__.py
@@ -26,6 +26,8 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
+    if CONF_MY_GAIN in config:
     cg.add(var.gain(config[CONF_MY_GAIN]))
     
     if CONF_MY_BLUETOOTH in config:
