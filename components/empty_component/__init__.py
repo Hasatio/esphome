5447ad8fff882f9defe3f2cac4300775fc4d9e51
wert95 <wert.95@hotmail.com>
2023-07-29 12:12:49 +0300

    Update __init__.py

--- components/empty_component/__init__.py
+++ components/empty_component/__init__.py
@@ -4,12 +4,11 @@ from esphome.components import output
 from esphome.const import CONF_ID
 
 empty_component_ns = cg.esphome_ns.namespace('empty_component') # esphome component adı "empty_component"
-EmptyComponent = empty_component_ns.class_('EmptyComponent', output.FloatOutput, cg.Component)
+EmptyComponent = empty_component_ns.class_('EmptyComponent', output.FloatOutput, cg.Component) # "EmptyComponent" sınıfı içeriğinin tanımlanması
 
-CONFIG_SCHEMA = cv.Schema({cv.GenerateID(): cv.declare_id(EmptyComponent)}).extend(cv.COMPONENT_SCHEMA)
-# CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend({cv.Required(CONF_ID): cv.declare_id(EmptyComponent)}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema({cv.GenerateID(): cv.declare_id(EmptyComponent)}).extend(cv.COMPONENT_SCHEMA) # komponent içeriğinde olanların tanımlaması
     
-def to_code(config):
+def to_code(config): # genel fonksiyon
     var = cg.new_Pvariable(config[CONF_ID])
-    yield output.register_output(var, config)
-    yield cg.register_component(var, config)
+    yield output.register_output(var, config) # output komponenti tanımı
+    yield cg.register_component(var, config) # genel komponent tanımı
