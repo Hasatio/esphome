7f9fc2b42aa43d2afce9520aaf37acde286dcc63
wert95 <wert.95@hotmail.com>
2023-07-29 04:51:21 +0300

    Update __init__.py

--- components/myi2c/__init__.py
+++ components/myi2c/__init__.py
@@ -29,7 +29,7 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_MY_SAMPLE): sensor.sensor_schema(
                     unit_of_measurement=UNIT_SAMPLE,
                     accuracy_decimals=0,
-                    device_class=DEVICE_CLASS_PRESSURE,
+                    device_class=DEVICE_CLASS_EMPTY,
                     state_class=STATE_CLASS_MEASUREMENT,
             ),
         }
