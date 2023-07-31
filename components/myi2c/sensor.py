b0e162da12e048f2ca6e3331f40def9ba995bc86
wert95 <wert.95@hotmail.com>
2023-08-01 02:27:47 +0300

    Update sensor.py

--- components/myi2c/sensor.py
+++ components/myi2c/sensor.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY
-from . import EmptySensorHub, CONF_HUB_ID
+from . import Myi2c, CONF_HUB_ID
 
 # DEPENDENCIES = ['empty_sensor_hub']
 
@@ -10,7 +10,7 @@ Sensor = sensor_ns.class_('Sensor', sensor.Sensor, cg.Nameable)
 
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 1).extend({
     cv.GenerateID(): cv.declare_id(Sensor),
-    cv.GenerateID(CONF_HUB_ID): cv.use_id(EmptySensorHub)
+    cv.GenerateID(CONF_HUB_ID): cv.use_id(Myi2c)
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
