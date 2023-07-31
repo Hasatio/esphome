86c4df69791259377637aafbd7a7e3b220c6d856
wert95 <wert.95@hotmail.com>
2023-08-01 02:33:55 +0300

    Update __init__.py

--- components/myi2c/__init__.py
+++ components/myi2c/__init__.py
@@ -23,32 +23,29 @@ DEVICE_CLASS_PRESSURE = "sample"
 myi2c_ns = cg.esphome_ns.namespace("myi2c") # esphome component adı "myi2c"
 Myi2c = myi2c_ns.class_("Myi2c", binary_sensor.BinarySensor, sensor.Sensor, cg.Component) # sınıf tanımlaması
 
-# CONFIG_SCHEMA = cv.All( # komponent içindekiler
-#     cv.Schema(
-#         {
-#             cv.GenerateID(): cv.declare_id(Myi2c), # id tanımlaması
-#             cv.Optional(CONF_MY_GAIN): cv.float_, # gain tanımlaması
-#             cv.Optional(CONF_MY_BLUETOOTH): cv.string, # bluetooth tanımlaması
-#             cv.Optional(CONF_MY_SAMPLE): sensor.sensor_schema( # sayaç sensör tanımlaması
-#                     unit_of_measurement=UNIT_SAMPLE, # sensörün birimi
-#                     accuracy_decimals=0, # sensörün sayısal gösterim şekli
-#                     device_class=DEVICE_CLASS_EMPTY, # sensör sınıfı
-#                     state_class=STATE_CLASS_MEASUREMENT,
-#             ),
-#             cv.Optional(CONF_MY_SAMPLE2): binary_sensor.binary_sensor_schema(),
-#         }
-#     )
-#     .extend(cv.COMPONENT_SCHEMA)
-# )
-CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(Myi2c),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.All( # komponent içindekiler
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(Myi2c), # id tanımlaması
+            cv.Optional(CONF_MY_GAIN): cv.float_, # gain tanımlaması
+            cv.Optional(CONF_MY_BLUETOOTH): cv.string, # bluetooth tanımlaması
+            # cv.Optional(CONF_MY_SAMPLE): sensor.sensor_schema( # sayaç sensör tanımlaması
+            #         unit_of_measurement=UNIT_SAMPLE, # sensörün birimi
+            #         accuracy_decimals=0, # sensörün sayısal gösterim şekli
+            #         device_class=DEVICE_CLASS_EMPTY, # sensör sınıfı
+            #         state_class=STATE_CLASS_MEASUREMENT,
+            # ),
+            # cv.Optional(CONF_MY_SAMPLE2): binary_sensor.binary_sensor_schema(),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 def to_code(config): # fonksiyon tanımlaması
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config) # komponent tanımlaması
-    yield sensor.register_sensor(var, config)
-    yield binary_sensor.register_binary_sensor(var, config)
+    # yield sensor.register_sensor(var, config)
+    # yield binary_sensor.register_binary_sensor(var, config)
     
     if CONF_MY_GAIN in config:
         cg.add(var.gain(config[CONF_MY_GAIN])) # gain fonksiyonu tanımlaması
