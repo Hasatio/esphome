b65a3ad5b124c1d63bfd1fd574c31d65767bbf21
wert95 <wert.95@hotmail.com>
2023-08-01 02:54:50 +0300

    Update sensor.py

--- components/myi2c/sensor.py
+++ components/myi2c/sensor.py
@@ -30,7 +30,7 @@ CONFIG_SCHEMA = (
 def to_code(config):
     parent = yield cg.get_variable(config[CONF_ID])
 
-    if CONF_SAMPLE in config:
+    if CONF_MY_SAMPLE in config:
         sens = yield sensor.new_sensor(config[CONF_MY_SAMPLE])
         cg.add(parent.sample(sens))
 
