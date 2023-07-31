a64059943508c4ad9efc4d0bceb11ae15a5ea24d
wert95 <wert.95@hotmail.com>
2023-07-31 16:08:47 +0300

    Update __init__.py

--- components/empty_binary_sensor /__init__.py
+++ components/empty_binary_sensor /__init__.py
@@ -1,19 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import binary_sensor
+from esphome.components import output
 from esphome.const import CONF_ID
 
-empty_binary_sensor_ns = cg.esphome_ns.namespace("empty_binary_sensor")
+empty_component_ns = cg.esphome_ns.namespace('empty_component') # esphome component adı "empty_component"
+EmptyComponent = empty_component_ns.class_('EmptyComponent', output.FloatOutput, cg.Component) # "EmptyComponent" sınıfı içeriğinin tanımlanması
 
-EmptyBinarySensor = empty_binary_sensor_ns.class_("EmptyBinarySensor", binary_sensor.BinarySensor, cg.Component)
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(EmptyComponent),
-    cv.GenerateID(): cv.declare_id(EmptyBinarySensor),
-}).extend(cv.COMPONENT_SCHEMA)
-
-
-def to_code(config):
+CONFIG_SCHEMA = cv.Schema({cv.GenerateID(): cv.declare_id(EmptyComponent)}).extend(cv.COMPONENT_SCHEMA) # komponent içeriğinde olanların tanımlaması
+    
+def to_code(config): # genel fonksiyon
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield binary_sensor.register_binary_sensor(var, config)
+    yield output.register_output(var, config) # output komponenti tanımı
+    yield cg.register_component(var, config) # genel komponent tanımı
