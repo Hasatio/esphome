96d2af03a2b9f094ac90cf1ef05e6e6a6066b2d9
wert95 <wert.95@hotmail.com>
2023-08-01 19:24:18 +0300

    Update myi2c.h

--- components/myi2c/myi2c.h
+++ components/myi2c/myi2c.h
@@ -26,7 +26,7 @@ void setup() override; // ayar fonksiyonu
 
 void loop() override; // döngü fonksiyonu
 
-void update();
+void update() override;
 
 void bluetooth(String b); // bluetooth fonksiyonu
 
