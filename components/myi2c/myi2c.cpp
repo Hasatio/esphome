a34f43dc5bc00dd11bd4e19fbb19365f7fbfcdb3
wert95 <wert.95@hotmail.com>
2023-08-02 02:13:02 +0300

    Update myi2c.cpp

--- components/myi2c/myi2c.cpp
+++ components/myi2c/myi2c.cpp
@@ -299,8 +299,8 @@ void Myi2c::loop() // döngü fonksiyonu
 
 void Myi2c::update()
 {  
-    this->my_sample_->publish_state(sayac);
-    this->my_sample_sec_->publish_state(sayac*1000/millis());
+    this->sample_->publish_state(sayac);
+    this->sample_sec_->publish_state(sayac*1000/millis());
 }
 
 } //namespace myi2c
