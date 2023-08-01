9570f58703140bde8d5b2b1dc4fdf832309f6a8e
wert95 <wert.95@hotmail.com>
2023-08-02 02:17:31 +0300

    Update sensor.py

--- components/myi2c/sensor.py
+++ components/myi2c/sensor.py
@@ -23,29 +23,21 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.use_id(Myi2c),
-            cv.Optional(CONF_MY_SAMPLE): sensor.sensor_schema( # sayaç sensör tanımlaması
-                unit_of_measurement=UNIT_SAMPLE, # sensörün birimi
-                accuracy_decimals=0, # sensörün sayısal gösterim şekli
-                device_class=DEVICE_CLASS_EMPTY, # sensör sınıfı
-                state_class=STATE_CLASS_MEASUREMENT,
-                entity_category=ENTITY_CATEGORY_NONE,
-            ),
-            cv.Optional(CONF_MY_SAMPLE_SEC): sensor.sensor_schema( # sayaç sensör tanımlaması
-                unit_of_measurement=UNIT_SAMPLE_SEC, # sensörün birimi
-                accuracy_decimals=0, # sensörün sayısal gösterim şekli
-                device_class=DEVICE_CLASS_EMPTY, # sensör sınıfı
-                state_class=STATE_CLASS_MEASUREMENT,
-                entity_category=ENTITY_CATEGORY_NONE,
-            )
-            .extend(cv.polling_component_schema("10s")),
-            cv.Optional(CONF_MY_UPTIME): sensor.sensor_schema(
-            unit_of_measurement=UNIT_SECOND,
-            icon=ICON_TIMER,
-            accuracy_decimals=0,
-            state_class=STATE_CLASS_TOTAL_INCREASING,
-            device_class=DEVICE_CLASS_DURATION,
-            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-            ).extend(cv.polling_component_schema("1s")),
+            # cv.Optional(CONF_MY_SAMPLE): sensor.sensor_schema( # sayaç sensör tanımlaması
+            #     unit_of_measurement=UNIT_SAMPLE, # sensörün birimi
+            #     accuracy_decimals=0, # sensörün sayısal gösterim şekli
+            #     device_class=DEVICE_CLASS_EMPTY, # sensör sınıfı
+            #     state_class=STATE_CLASS_MEASUREMENT,
+            #     entity_category=ENTITY_CATEGORY_NONE,
+            # ),
+            # cv.Optional(CONF_MY_SAMPLE_SEC): sensor.sensor_schema( # sayaç sensör tanımlaması
+            #     unit_of_measurement=UNIT_SAMPLE_SEC, # sensörün birimi
+            #     accuracy_decimals=0, # sensörün sayısal gösterim şekli
+            #     device_class=DEVICE_CLASS_EMPTY, # sensör sınıfı
+            #     state_class=STATE_CLASS_MEASUREMENT,
+            #     entity_category=ENTITY_CATEGORY_NONE,
+            # )
+            # .extend(cv.polling_component_schema("10s")),
         }
     )
 )
@@ -53,11 +45,11 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_ID])
 
-    if CONF_MY_SAMPLE in config:
-        sens = await sensor.new_sensor(config[CONF_MY_SAMPLE])
-        cg.add(parent.sample(sens))
-    if CONF_MY_SAMPLE_SEC in config:
-        sens = await sensor.new_sensor(config[CONF_MY_SAMPLE_SEC])
-        cg.add(parent.sample_sec(sens))
+    # if CONF_MY_SAMPLE in config:
+    #     sens = await sensor.new_sensor(config[CONF_MY_SAMPLE])
+    #     cg.add(parent.sample(sens))
+    # if CONF_MY_SAMPLE_SEC in config:
+    #     sens = await sensor.new_sensor(config[CONF_MY_SAMPLE_SEC])
+    #     cg.add(parent.sample_sec(sens))
 
 
