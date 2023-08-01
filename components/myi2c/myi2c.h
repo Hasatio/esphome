0e179d86eca394e3198657b01e6bcb02e0277f2e
wert95 <wert.95@hotmail.com>
2023-08-01 03:20:39 +0300

    Update myi2c.h

--- components/myi2c/myi2c.h
+++ components/myi2c/myi2c.h
@@ -6,7 +6,6 @@
 #include "esphome/core/defines.h"
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
 #include <Adafruit_ADS1X15.h>
 #include <Adafruit_ADXL345_U.h>
 #include <Adafruit_MAX1704X.h>
