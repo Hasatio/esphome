2ac5449d49f50e3618206b83cec54b19a4b676fa
wert95 <wert.95@hotmail.com>
2023-08-01 18:06:25 +0300

    Update myi2c.h

--- components/myi2c/myi2c.h
+++ components/myi2c/myi2c.h
@@ -24,9 +24,9 @@ float get_setup_priority() const override { return esphome::setup_priority::PROC
 
 void setup() override; // ayar fonksiyonu 
 
-void loop(); // döngü fonksiyonu
+void loop() override; // döngü fonksiyonu
 
-void update(); 
+void update() override; 
 
 void bluetooth(String b); // bluetooth fonksiyonu
 
