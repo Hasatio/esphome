6413d5c8701d77220545b62914db587b2eefb095
wert95 <wert.95@hotmail.com>
2023-08-01 05:22:49 +0300

    Update myi2c.cpp

--- components/myi2c/myi2c.cpp
+++ components/myi2c/myi2c.cpp
@@ -1,9 +1,22 @@
 #include "myi2c.h"
 
+#ifdef USE_ESP32
+#if defined(USE_ESP32_VARIANT_ESP32)
+// there is no official API available on the original ESP32
+extern "C" {
+uint8_t temprature_sens_read();
+}
+#elif defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
+#include "driver/temp_sensor.h"
+#endif  // USE_ESP32_VARIANT
+#endif  // USE_ESP32
+#ifdef USE_RP2040
+#include "Arduino.h"
+#endif  // USE_RP2040
+
 namespace esphome {
 namespace myi2c {
     
-    // static const char *const TAG = "myi2c.sensor";
 static const char *TAG = "myi2c.sensor";
 
     BluetoothSerial SerialBT; // bluetooth yeni adlandırması
@@ -210,6 +223,41 @@ void Myi2c::setup() // ayar fonksiyonu
     
 void Myi2c::loop() // döngü fonksiyonu
 {
+    
+  float temperature = NAN;
+  bool success = false;
+#ifdef USE_ESP32
+#if defined(USE_ESP32_VARIANT_ESP32)
+  uint8_t raw = temprature_sens_read();
+  ESP_LOGV(TAG, "Raw temperature value: %d", raw);
+  temperature = (raw - 32) / 1.8f;
+  success = (raw != 128);
+#elif defined(USE_ESP32_VARIANT_ESP32C3) || defined(USE_ESP32_VARIANT_ESP32S2) || defined(USE_ESP32_VARIANT_ESP32S3)
+  temp_sensor_config_t tsens = TSENS_CONFIG_DEFAULT();
+  temp_sensor_set_config(tsens);
+  temp_sensor_start();
+#if defined(USE_ESP32_VARIANT_ESP32S3) && (ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 4, 3))
+#error \
+    "ESP32-S3 internal temperature sensor requires ESP IDF V4.4.3 or higher. See https://github.com/esphome/issues/issues/4271"
+#endif
+  esp_err_t result = temp_sensor_read_celsius(&temperature);
+  temp_sensor_stop();
+  success = (result == ESP_OK);
+#endif  // USE_ESP32_VARIANT
+#endif  // USE_ESP32
+#ifdef USE_RP2040
+  temperature = analogReadTemp();
+  success = (temperature != 0.0f);
+#endif  // USE_RP2040
+  if (success && std::isfinite(temperature)) {
+    this->publish_state(temperature);
+  } else {
+    ESP_LOGD(TAG, "Ignoring invalid temperature (success=%d, value=%.1f)", success, temperature);
+    if (!this->has_state()) {
+      this->publish_state(NAN);
+    }
+  }
+    
     if(SerialBT.available())
       {
         mygain = float(SerialBT.read());
@@ -249,7 +297,7 @@ void Myi2c::loop() // döngü fonksiyonu
     voltage = maxlipo.cellVoltage();
     percentage = maxlipo.cellPercent();
     
-    data = data + String(x) + "," + String(y) + "," + String(z) + "," + String(voltage) + "," + String(percentage);
+    data = data + String(x) + "," + String(y) + "," + String(z) + "," + String(voltage) + "," + String(percentage) + "," String(temperature);
 
     SerialBT.println(data);
     data = "";
