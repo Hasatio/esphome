19ad0650fc39cf99adaafcc237fd1a1f9e6cfe3b
wert95 <wert.95@hotmail.com>
2023-08-02 03:36:05 +0300

    Update sensor.py

--- components/myi2c/sensor.py
+++ components/myi2c/sensor.py
@@ -44,7 +44,7 @@ async def to_code(config):
 
     if CONF_MY_SAMPLE in config:
         sens = await sensor.new_sensor(config[CONF_MY_SAMPLE])
-        cg.add(parent.sample_all(sens))
+        cg.add(parent.sample(sens))
     if CONF_MY_SAMPLE_SEC in config:
         sens = await sensor.new_sensor(config[CONF_MY_SAMPLE_SEC])
         cg.add(parent.sample_sec(sens))
