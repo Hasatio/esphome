7688130d9090fd87a9892ba74724866d62109818
wert95 <wert.95@hotmail.com>
2023-08-02 18:22:48 +0300

    Update sensor.py

--- components/myi2c/sensor.py
+++ components/myi2c/sensor.py
@@ -17,13 +17,10 @@ from . import myi2c_ns, Myi2c, CONF_MY_ID, CONF_MY_SAMPLE, CONF_MY_SAMPLE_SEC, U
 
 DEPENDENCIES = ["myi2c"] # gerekli olan komponent, bu olmadan tanımlı sensörler kullanılamaz.
 
-sensor_ns = cg.esphome_ns.namespace('sensor')
-Sensor = sensor_ns.class_('Sensor', sensor.Sensor, cg.Nameable)
-
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.GenerateID(): cv.use_id(Myi2c),
+            cv.GenerateID(CONF_MY_ID): cv.use_id(Myi2c),
             cv.Optional(CONF_MY_SAMPLE): sensor.sensor_schema( # sayaç sensör tanımlaması
                 unit_of_measurement=UNIT_SAMPLE, # sensörün birimi
                 accuracy_decimals=0, # sensörün sayısal gösterim şekli
@@ -39,30 +36,18 @@ CONFIG_SCHEMA = (
                 entity_category=ENTITY_CATEGORY_NONE,
             ).extend(cv.polling_component_schema("10s")),
         }
-    ).extend(cv.COMPONENT_SCHEMA)
+    )
 )
 
-# def to_code(config):
-#     parent = yield cg.get_variable(config[CONF_ID])
+async def to_code(config):
+    # parent = await cg.get_variable(config[CONF_ID])
+    parent = await cg.get_variable(config[CONF_MY_ID])
 
-#     if CONF_MY_SAMPLE in config:
-#         sens = yield sensor.new_sensor(config[CONF_MY_SAMPLE])
-#         cg.add(parent.sample(sens))
-#     if CONF_MY_SAMPLE_SEC in config:
-#         sens = yield sensor.new_sensor(config[CONF_MY_SAMPLE_SEC])
-#         cg.add(parent.sample_sec(sens))
-def to_code(config):
-    paren = yield cg.get_variable(config[CONF_MY_ID])
-    var = cg.new_Pvariable(config[CONF_ID])
-    
-    yield sensor.register_sensor(var, config)
-    
-    cg.add(paren.register_sensor(var))
     if CONF_MY_SAMPLE in config:
-        sens = yield sensor.new_sensor(config[CONF_MY_SAMPLE])
-        cg.add(paren.sample(sens))
+        sens = await sensor.new_sensor(config[CONF_MY_SAMPLE])
+        cg.add(parent.sample(sens))
     if CONF_MY_SAMPLE_SEC in config:
-        sens = yield sensor.new_sensor(config[CONF_MY_SAMPLE_SEC])
-        cg.add(paren.sample_sec(sens))
+        sens = await sensor.new_sensor(config[CONF_MY_SAMPLE_SEC])
+        cg.add(parent.sample_sec(sens))
 
 
