a26b2d508d9d1ca1cd18a48b43c8dfaa52a9126d
wert95 <wert.95@hotmail.com>
2023-08-07 16:18:35 +0300

    Update myi2c.cpp

--- components/myi2c/myi2c.cpp
+++ components/myi2c/myi2c.cpp
@@ -184,27 +184,29 @@ void Myi2c::setup() // ayar fonksiyonu
     }
 
     /* Set the range to whatever is appropriate for your project */
-    //accel.setRange(ADXL345_RANGE_16_G);  ///< +/- 16g
-    //accel.setRange(ADXL345_RANGE_8_G);  ///< +/- 8g
-    //accel.setRange(ADXL345_RANGE_4_G);  ///< +/- 4g
-    accel.setRange(ADXL345_RANGE_2_G);  ///< +/- 2g (default value)
+    //  ADXL345_RANGE_16_G ///< +/- 16g
+    //  ADXL345_RANGE_8_G  ///< +/- 8g
+    //  ADXL345_RANGE_4_G  ///< +/- 4g
+    //  ADXL345_RANGE_2_G  ///< +/- 2g (default value)
+    accel.setRange(ADXL345_RANGE_2_G);
     
-    //accel.setDataRate(ADXL345_DATARATE_3200_HZ);  ///< 1600Hz Bandwidth   140�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_1600_HZ);  ///<  800Hz Bandwidth    90�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_800_HZ);  ///<  400Hz Bandwidth   140�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_400_HZ);  ///<  200Hz Bandwidth   140�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_200_HZ);  ///<  100Hz Bandwidth   140�A IDD
-    accel.setDataRate(ADXL345_DATARATE_100_HZ);  ///<   50Hz Bandwidth   140�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_50_HZ);   ///<   25Hz Bandwidth    90�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_25_HZ);   ///< 12.5Hz Bandwidth    60�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_12_5_HZ); ///< 6.25Hz Bandwidth    50�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_6_25HZ);  ///< 3.13Hz Bandwidth    45�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_3_13_HZ); ///< 1.56Hz Bandwidth    40�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_1_56_HZ); ///< 0.78Hz Bandwidth    34�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_0_78_HZ); ///< 0.39Hz Bandwidth    23�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_0_39_HZ); ///< 0.20Hz Bandwidth    23�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_0_20_HZ); ///< 0.10Hz Bandwidth    23�A IDD
-    //accel.setDataRate(ADXL345_DATARATE_0_10_HZ); ///< 0.05Hz Bandwidth    23�A IDD (default value)
+    //  ADXL345_DATARATE_3200_HZ  ///< 1600Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_1600_HZ  ///<  800Hz Bandwidth    90�A IDD
+    //  ADXL345_DATARATE_800_HZ  ///<  400Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_400_HZ  ///<  200Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_200_HZ  ///<  100Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_100_HZ  ///<   50Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_50_HZ   ///<   25Hz Bandwidth    90�A IDD
+    //  ADXL345_DATARATE_25_HZ   ///< 12.5Hz Bandwidth    60�A IDD
+    //  ADXL345_DATARATE_12_5_HZ ///< 6.25Hz Bandwidth    50�A IDD
+    //  ADXL345_DATARATE_6_25HZ  ///< 3.13Hz Bandwidth    45�A IDD
+    //  ADXL345_DATARATE_3_13_HZ ///< 1.56Hz Bandwidth    40�A IDD
+    //  ADXL345_DATARATE_1_56_HZ ///< 0.78Hz Bandwidth    34�A IDD
+    //  ADXL345_DATARATE_0_78_HZ ///< 0.39Hz Bandwidth    23�A IDD
+    //  ADXL345_DATARATE_0_39_HZ ///< 0.20Hz Bandwidth    23�A IDD
+    //  ADXL345_DATARATE_0_20_HZ ///< 0.10Hz Bandwidth    23�A IDD
+    //  ADXL345_DATARATE_0_10_HZ ///< 0.05Hz Bandwidth    23�A IDD (default value)
+    accel.setDataRate(ADXL345_DATARATE_100_HZ);
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
