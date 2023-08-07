311245071414d086c0c2cb2910caddef94e8f3b7
wert95 <wert.95@hotmail.com>
2023-08-07 16:08:57 +0300

    Update myi2c.cpp

--- components/myi2c/myi2c.cpp
+++ components/myi2c/myi2c.cpp
@@ -134,23 +134,42 @@ void Myi2c::setup() // ayar fonksiyonu
     // functions, but be careful never to exceed VDD +0.3V max, or to
     // exceed the upper and lower limits if you adjust the input range!
     // Setting these values incorrectly may destroy your ADC!
-    //                                                                ADS1015  ADS1115
-    //                                                                -------  -------
-    // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-    // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-    // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-    // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-    // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-    // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+    //
+    //                                          ADS1015          ADS1115
+    //                                          -------          -------
+    // GAIN_TWOTHIRDS  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+    // GAIN_ONE        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+    // GAIN_TWO        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+    // GAIN_FOUR       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+    // GAIN_EIGHT      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+    // GAIN_SIXTEEN    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
     ads1.setGain(GAIN_ONE); 
     ads2.setGain(GAIN_ONE); 
     ads3.setGain(GAIN_ONE);
     ads4.setGain(GAIN_ONE);
-    // ads.setDataRate(RATE_ADS1115_860SPS);
+    
+    // RATE_ADS1115_8SPS (0x0000)   ///< 8 samples per second
+    // RATE_ADS1115_16SPS (0x0020)  ///< 16 samples per second
+    // RATE_ADS1115_32SPS (0x0040)  ///< 32 samples per second
+    // RATE_ADS1115_64SPS (0x0060)  ///< 64 samples per second
+    // RATE_ADS1115_128SPS (0x0080) ///< 128 samples per second (default)
+    // RATE_ADS1115_250SPS (0x00A0) ///< 250 samples per second
+    // RATE_ADS1115_475SPS (0x00C0) ///< 475 samples per second
+    // RATE_ADS1115_860SPS (0x00E0) ///< 860 samples per second
     ads1.setDataRate(RATE_ADS1115_860SPS);
     ads2.setDataRate(RATE_ADS1115_860SPS);
     ads3.setDataRate(RATE_ADS1115_860SPS);
     ads4.setDataRate(RATE_ADS1115_860SPS);
+    
+    // ADS1X15_REG_CONFIG_MUX_DIFF_0_1 (0x0000) ///< Differential P = AIN0, N = AIN1 (default)
+    // ADS1X15_REG_CONFIG_MUX_DIFF_0_3 (0x1000) ///< Differential P = AIN0, N = AIN3
+    // ADS1X15_REG_CONFIG_MUX_DIFF_1_3 (0x2000) ///< Differential P = AIN1, N = AIN3
+    // ADS1X15_REG_CONFIG_MUX_DIFF_2_3 (0x3000) ///< Differential P = AIN2, N = AIN3
+    // ADS1X15_REG_CONFIG_MUX_SINGLE_0 (0x4000) ///< Single-ended AIN0
+    // ADS1X15_REG_CONFIG_MUX_SINGLE_1 (0x5000) ///< Single-ended AIN1
+    // ADS1X15_REG_CONFIG_MUX_SINGLE_2 (0x6000) ///< Single-ended AIN2
+    // ADS1X15_REG_CONFIG_MUX_SINGLE_3 (0x7000) ///< Single-ended AIN3
+    // ads.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1);
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
