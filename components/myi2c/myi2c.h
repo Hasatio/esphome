87d88d139be6e3c14ce4fd44a3a5f759a572e45d
wert95 <wert.95@hotmail.com>
2023-08-09 16:17:05 +0300

    Update myi2c.h

--- components/myi2c/myi2c.h
+++ components/myi2c/myi2c.h
@@ -2,10 +2,7 @@
 
 #include "esphome.h"
 #include "esphome/core/log.h"
-#include "esphome/core/time.h"
-#include "esphome/core/defines.h"
 #include "esphome/core/component.h"
-#include "esphome/core/helpers.h"
 #include "esphome/core/hal.h"
 #include "esphome/components/sensor/sensor.h"
 #include <Adafruit_ADS1X15.h>
@@ -32,7 +29,6 @@ void bluetooth(String b); // bluetooth fonksiyonu
 
 void gain(float g); // kazanç fonksiyonu
 
-#ifdef USE_SENSOR
 void sample(sensor::Sensor *sample) // sayaç sensörü fonksiyonu
 { 
     sample_ = sample;
@@ -42,14 +38,11 @@ void sample_sec(sensor::Sensor *sample_sec) // sayaç sensörü fonksiyonu
 { 
     sample_sec_ = sample_sec;
 }
-#endif
 
 protected:
 
-#ifdef USE_SENSOR
 sensor::Sensor *sample_{nullptr}; // sensör değişkeni
 sensor::Sensor *sample_sec_{nullptr}; // sensör değişkeni
-#endif
 
 }; // class Myi2c
 } //namespace myi2c
