5621e3c1183e17527bfb389601b04133abcfc4ac
wert95 <wert.95@hotmail.com>
2023-08-10 20:28:02 +0300

    Update __init__.py

--- components/bme680_bsec/__init__.py
+++ components/bme680_bsec/__init__.py
@@ -74,6 +74,6 @@ async def to_code(config):
     )
     cg.add_library(
         "BSEC2 Software Library",
-        "v1.4.2200",
+        "1.4.2200",
         # "https://github.com/BoschSensortec/Bosch-BSEC2-Library.git",
     )
