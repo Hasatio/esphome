288235df1612d6db9d9e4c2278fad3f9670442ee
wert95 <wert.95@hotmail.com>
2023-08-10 20:12:09 +0300

    Update __init__.py

--- components/bme680_bsec/__init__.py
+++ components/bme680_bsec/__init__.py
@@ -67,11 +67,11 @@ async def to_code(config):
 
     cg.add_define("USE_BSEC")
     #cg.add_library("boschsensortec/BSEC Software Library", "1.6.1480")
-    # cg.add_library(
-    #     "BME68x Sensor library",
-    #     "1.1.40406",
+    cg.add_library(
+        "BME68x Sensor library",
+        "1.1.40406",
     #     "https://github.com/BoschSensortec/Bosch-BME68x-Library.git",
-    # )
+    )
     # cg.add_library(
     #     "BSEC2 Software Library",
     #     "1.4.2200",
