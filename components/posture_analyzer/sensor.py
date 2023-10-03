a59c233d2693b322c1bba6c8694ef7510854ce48
wert95 <wert.95@hotmail.com>
2023-10-03 14:47:05 +0300

    Update sensor.py

--- components/posture_analyzer/sensor.py
+++ components/posture_analyzer/sensor.py
@@ -5,14 +5,14 @@ from esphome.const import (
     CONF_ID,
 )
 
-from . import component_ns, Component, CONF_MY_SAMPLE, CONF_MY_SAMPLE_SEC, UNIT_SAMPLE, UNIT_SAMPLE_SEC
+from . import component_ns, MyComponent, CONF_MY_SAMPLE, CONF_MY_SAMPLE_SEC, UNIT_SAMPLE, UNIT_SAMPLE_SEC
 
 DEPENDENCIES = ["posture_analyzer"] # gerekli olan komponent, bu olmadan tanımlı sensörler kullanılamaz.
 
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.GenerateID(): cv.use_id(Component),
+            cv.GenerateID(): cv.use_id(MyComponent),
             cv.Optional(CONF_MY_SAMPLE): sensor.sensor_schema( # sayaç sensör tanımlaması
                 unit_of_measurement=UNIT_SAMPLE, # sensörün birimi
                 accuracy_decimals=0, # sensörün sayısal gösterim şekli
