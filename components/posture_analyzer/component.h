428dd837b8cd80f2940df8ea6ebbd83d5d8dd839
wert95 <wert.95@hotmail.com>
2023-10-03 12:51:08 +0300

    Update component.h

--- components/posture_analyzer/component.h
+++ components/posture_analyzer/component.h
@@ -5,9 +5,7 @@
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 #include "esphome/components/sensor/sensor.h"
-#include <Wire.h>
 #include <Adafruit_I2CDevice.h>
-#include <Adafruit_BusIO_Register_h>
 #include <Adafruit_ADS1X15.h>
 #include <Adafruit_ADXL345_U.h>
 #include <Adafruit_MAX1704X.h>
