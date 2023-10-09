bc38ee9bbfea3a190e9b00d4f433cc28fa7ca0d1
wert95 <wert.95@hotmail.com>
2023-10-09 15:55:26 +0300

    .

--- components/water_quality/component.h
+++ components/water_quality/component.h
@@ -31,11 +31,11 @@ void pump(String PT[6],uint8_t PCX[8],uint8_t PCY[8],uint8_t PM[4],uint8_t PD[4]
 void dat(std::vector<uint8_t> &data);
 };
 
-void Pump_0_Total(sensor::Sensor *pt) Pump_0_Total_ = pt;
+void pt(sensor::Sensor *Pump_0_Total) Pump_0_Total_ = pt;
 // void Pump_1_Total(sensor::Sensor *pt) Pump_1_Total_ = pt;
 // void Pump_2_Total(sensor::Sensor *pt) Pump_2_Total_ = pt;
 // void Pump_3_Total(sensor::Sensor *pt) Pump_3_Total_ = pt;
-void Pump_0_Status(sensor::Sensor *ps) Pump_0_Status_ = ps;
+void ps(sensor::Sensor *Pump_0_Status) Pump_0_Status_ = ps;
 // void Pump_1_Status(sensor::Sensor *ps) Pump_1_Status_ = ps;   
 // void Pump_2_Status(sensor::Sensor *ps) Pump_2_Status_ = ps; 
 // void Pump_3_Status(sensor::Sensor *ps) Pump_3_Status_ = ps;
--- components/water_quality/sensor.py
+++ components/water_quality/sensor.py
@@ -43,11 +43,11 @@ async def to_code(config):
 
     if CONF_PUMP_TOTAL in config:
         sens = await sensor.new_sensor(config[CONF_PUMP_TOTAL])
-        cg.add(parent.Pump_0_Total(sens))
+        cg.add(parent.pt(sens))
         # cg.add(parent.Pump_1_Total(sens))
         # cg.add(parent.Pump_2_Total(sens))
         # cg.add(parent.Pump_3_Total(sens))
         
     if CONF_PUMP_STATUS in config:
         sens = await sensor.new_sensor(config[CONF_PUMP_STATUS])
-        cg.add(parent.Pump_0_Status(sens))
+        cg.add(parent.ps(sens))
