2dad2157f2911246b155ebce1bcd734fc54b70ca
wert95 <wert.95@hotmail.com>
2023-10-10 15:47:16 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -4,6 +4,7 @@ from esphome.components import sensor
 from esphome.const import (
     CONF_ID, 
     CONF_DATA,
+    CONF_CUSTOM_COMMAND,
 ) 
 
 AUTO_LOAD = ["sensor"]
@@ -22,18 +23,19 @@ MyComponent = component_ns.class_("MyComponent", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MyComponent),
-    cv.Optional(CONF_USER_CHARACTERS): cv.All(
-            cv.ensure_list(
-                cv.Schema(
-                    {
-                        cv.Required(CONF_DATA): cv.All(
-                            cv.ensure_list(cv.uint8_t)
-                        ),
-                    }
-                ),
-            ),
-            cv.Length(max=8),
-        ),
+    # cv.Optional(CONF_USER_CHARACTERS): cv.All(
+    #         cv.ensure_list(
+    #             cv.Schema(
+    #                 {
+    #                     cv.Required(CONF_DATA): cv.All(
+    #                         cv.ensure_list(cv.uint8_t)
+    #                     ),
+    #                 }
+    #             ),
+    #         ),
+    #         cv.Length(max=8),
+    #     ),
+    cv.Optional(CONF_CUSTOM_COMMAND): cv.ensure_list(cv.hex_uint8_t),
 }).extend(cv.COMPONENT_SCHEMA)
 
 # def to_code(config):
@@ -44,9 +46,13 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    if CONF_USER_CHARACTERS in config:
-        for usr in config[CONF_USER_CHARACTERS]:
-            cg.add(var.set_user_defined_char(usr[CONF_DATA]))
+    # if CONF_USER_CHARACTERS in config:
+    #     for usr in config[CONF_USER_CHARACTERS]:
+    #         cg.add(var.set_user_defined_char(usr[CONF_DATA]))
+    
+    if CONF_CUSTOM_COMMAND in config:
+        cg.add(var.set_custom_data(config[CONF_CUSTOM_COMMAND]))
+
     
     cg.add_library("Wire", None)
     cg.add_library("SPI", None)
