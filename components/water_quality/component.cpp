5fbb74d2979e853b2234be7171c758ca02678c89
wert95 <wert.95@hotmail.com>
2023-10-10 17:43:53 +0300

    Update component.cpp

--- components/water_quality/component.cpp
+++ components/water_quality/component.cpp
@@ -5,7 +5,7 @@ namespace water_quality {
 
 static const char *TAG = "mysensor";
 
-uint8_t data2[] = {1,2,3,4};
+uint8_t data2[] = {11,22,33,44};
 void MyComponent::setup() 
 {
 
@@ -16,7 +16,7 @@ void MyComponent::loop()
 
     // Pump_CalibX = int(custom_data);
         ESP_LOGD(TAG,"data1 = %d", *data1);
-        ESP_LOGD(TAG,"data2 = %d", data2[]);
+        ESP_LOGD(TAG,"data2 = %d", data2[0]);
 
 delay(1000);
 }
