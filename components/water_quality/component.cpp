28b64c81f7e1243cb9d4aeb2715808c50e8d2242
wert95 <wert.95@hotmail.com>
2023-10-10 15:20:03 +0300

    Update component.cpp

--- components/water_quality/component.cpp
+++ components/water_quality/component.cpp
@@ -351,8 +351,8 @@ void MyComponent::loop()
         this->AnOut_Status_->publish_state(AnOut_LvlPerc[0]);
         this->AnOut_Status_->publish_state(AnOut_LvlPerc[1]);
     }
-    Pump_CalibX = int(custom_data);
-        ESP_LOGD(TAG,"pwm = %d", Pump_CalibX[0]);
+    // Pump_CalibX = int(custom_data);
+        ESP_LOGD(TAG,"pwm = %c", custom_data);
     // if (this->AnOut_0_Temp_ != nullptr) this->AnOut_0_Temp_->publish_state(AnOut_Temp);
     // if (this->AnOut_1_Vcc_ != nullptr) this->AnOut_1_Vcc_->publish_state(AnOut_Vcc);
     // if (this->AnOut_2_LvlPerc_ != nullptr) this->AnOut_2_LvlPerc_->publish_state(AnOut_LvlPerc[0]);
