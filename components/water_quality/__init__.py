483cb481477ac610448a4b83471de1f4834128b7
wert95 <wert.95@hotmail.com>
2023-10-11 11:57:20 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -50,7 +50,7 @@ CONFIG_SCHEMA = cv.Schema({
 #     var = cg.new_Pvariable(config[CONF_ID])
 #     yield cg.register_component(var, config)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     
