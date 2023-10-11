3e52c27ab6a70f02727b52863360a46c1d5d5041
wert95 <wert.95@hotmail.com>
2023-10-11 14:10:39 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -60,12 +60,12 @@ async def to_code(config):
     
     if CONF_CALIBRATION in config:
         
-        # for usr in config[CONF_CALIBRATION]:
-        #     cg.add(var.calibration(usr[CONF_X]))
+        for usr in config[CONF_CALIBRATION]:
+            cg.add(var.calibration(usr[CONF_X]))
     
-        for conf in config[CONF_CALIBRATION]:
-            cg.add(var.calibration(conf[CONF_X]))
-            await cg.register_component(var, conf)
+        # for conf in config[CONF_CALIBRATION]:
+        #     cg.add(var.calibration(conf[CONF_X]))
+        #     await cg.register_component(var, conf)
     
     # if CONF_CALIBRATION in config:
     #     cg.add(var.calibration(config[CONF_X]))
