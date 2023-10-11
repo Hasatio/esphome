194c0f022e06a45a9aa943424750f0a43a070b13
wert95 <wert.95@hotmail.com>
2023-10-11 14:20:17 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -31,7 +31,7 @@ CONFIG_SCHEMA = (
                 # cv.ensure_list(
                     cv.Schema(
                         {
-                            cv.Required(CONF_X): cv.All(
+                            cv.Optional(CONF_X): cv.All(
                                 cv.ensure_list(cv.uint8_t),
                                 cv.Length(min=8, max=8),
                             ),
