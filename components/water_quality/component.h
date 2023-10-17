5659d84d0978c416eb6f5794cc41b1284e4b974b
wert95 <wert.95@hotmail.com>
2023-10-17 22:51:49 +0300

    Update component.h

--- components/water_quality/component.h
+++ components/water_quality/component.h
@@ -77,17 +77,21 @@ sensor::Sensor *DigOut_Status_{nullptr};
 
 
 template<typename... Ts> class DoseVolumeAction : public Action<Ts...> {
- public:
-  DoseVolumeAction(MyComponent *dose) : dose_(dose) {}
+    public:
+    DoseVolumeAction(MyComponent *dose) : dose_(dose) {}
 
-  void play(Ts... x) override { this->dose_->dose(this->data_.value(x...));Pump_Dose[0]=this->data_.value(x...); }
+    void play(Ts... x) override 
+    { 
+        this->dose_->dose(this->data_.value(x...));
+        dd2=this->data_.value(x...); 
+    }
 //   void play(Ts... x) override { this->dose_->Pump_Dose; }
 //   void play(Ts... x) override { this->dose_->Pump_Dose; }
 //   TEMPLATABLE_VALUE(uint8_t, data)
-  TEMPLATABLE_VALUE(double, data)
+    TEMPLATABLE_VALUE(double, data)
 
- protected:
-  MyComponent *dose_;
+    protected:
+    MyComponent *dose_;
   
 };
 
