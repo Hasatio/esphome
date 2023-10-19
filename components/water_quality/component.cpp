0d57c4853e1d1d062174a1dd623f32cb1c41f75a
wert95 <wert.95@hotmail.com>
2023-10-19 06:59:56 +0300

    Update component.cpp

--- components/water_quality/component.cpp
+++ components/water_quality/component.cpp
@@ -15,18 +15,18 @@ void MyComponent::setup()
 void MyComponent::loop() 
 {
 
-    for (size_t i = 0; i < (sizeof(Pump_Dose) / sizeof(Pump_Dose[0])); i++)
+    for (size_t i = 0; i < (sizeof(Pump_Calib_X1) / sizeof(Pump_Calib_X1[0])); i++)
     {
-    //     ESP_LOGD(TAG,"x1[%d] = %d", i, Pump_Calib_X1[i]);
-    //     ESP_LOGD(TAG,"y1[%d] = %d", i, Pump_Calib_Y1[i]);
-    //     ESP_LOGD(TAG,"x2[%d] = %d", i, Pump_Calib_X2[i]);
-    //     ESP_LOGD(TAG,"y2[%d] = %d", i, Pump_Calib_Y2[i]);
-    //     ESP_LOGD(TAG,"x3[%d] = %d", i, Pump_Calib_X3[i]);
-    //     ESP_LOGD(TAG,"y3[%d] = %d", i, Pump_Calib_Y3[i]);
-    //     ESP_LOGD(TAG,"x4[%d] = %d", i, Pump_Calib_X4[i]);
-    //     ESP_LOGD(TAG,"y4[%d] = %d", i, Pump_Calib_Y4[i]);
+        ESP_LOGD(TAG,"x1[%d] = %d", i, Pump_Calib_X1[i]);
+        ESP_LOGD(TAG,"y1[%d] = %d", i, Pump_Calib_Y1[i]);
+        ESP_LOGD(TAG,"x2[%d] = %d", i, Pump_Calib_X2[i]);
+        ESP_LOGD(TAG,"y2[%d] = %d", i, Pump_Calib_Y2[i]);
+        ESP_LOGD(TAG,"x3[%d] = %d", i, Pump_Calib_X3[i]);
+        ESP_LOGD(TAG,"y3[%d] = %d", i, Pump_Calib_Y3[i]);
+        ESP_LOGD(TAG,"x4[%d] = %d", i, Pump_Calib_X4[i]);
+        ESP_LOGD(TAG,"y4[%d] = %d", i, Pump_Calib_Y4[i]);
 
-        ESP_LOGD(TAG,"data[%d] = %d", i, Pump_Dose[i]);
+        // ESP_LOGD(TAG,"data[%d] = %d", i, Pump_Dose[i]);
     }
     
         
