c7b56837b35456c5566728f241980a335ba83cde
wert95 <wert.95@hotmail.com>
2023-10-19 06:50:32 +0300

    Update component.cpp

--- components/water_quality/component.cpp
+++ components/water_quality/component.cpp
@@ -15,7 +15,7 @@ void MyComponent::setup()
 void MyComponent::loop() 
 {
 
-    for (int i = 0; i < 4; i++)
+    for (sizeint_t i = 0; i < 4; i++)
     {
     //     ESP_LOGD(TAG,"x1[%d] = %d", i, Pump_Calib_X1[i]);
     //     ESP_LOGD(TAG,"y1[%d] = %d", i, Pump_Calib_Y1[i]);
@@ -26,7 +26,7 @@ void MyComponent::loop()
     //     ESP_LOGD(TAG,"x4[%d] = %d", i, Pump_Calib_X4[i]);
     //     ESP_LOGD(TAG,"y4[%d] = %d", i, Pump_Calib_Y4[i]);
 
-        ESP_LOGD(TAG,"data[%d] = %d", i, Pump_Dose[i]);
+        // ESP_LOGD(TAG,"data[%d] = %d", i, Pump_Dose[i]);
     }
     
         
