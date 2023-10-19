61612094838d7a49430d01725cb7f1546a39dbbc
wert95 <wert.95@hotmail.com>
2023-10-19 21:12:29 +0300

    Update component.h

--- components/water_quality/component.h
+++ components/water_quality/component.h
@@ -18,7 +18,7 @@ float get_setup_priority() const override { return esphome::setup_priority::PROC
 uint16_t AnIn_TempRes = 1000; //temperature sensor model pt1000 and its resistance is 1k
 float AnOut_Vcc, AnOut_Temp, TempRes;
 uint8_t DigIn_FilterCoeff[4][10];
-uint8_t dd,dd2[];
+uint8_t dd,dd2[8];
 
 void setup() override;
 void loop() override;
