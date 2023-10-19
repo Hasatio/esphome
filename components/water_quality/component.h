42539a73d0b8881673451fb226b8e98b18bbe423
wert95 <wert.95@hotmail.com>
2023-10-19 06:35:56 +0300

    Update component.h

--- components/water_quality/component.h
+++ components/water_quality/component.h
@@ -105,16 +105,16 @@ template<typename... Ts> class DoseVolumeAction : public Action<Ts...> {
     public:
     DoseVolumeAction(MyComponent *parent) : parent_(parent){};
 
-    void set_d(const std::vector<uint8_t> &d) { data_ = d; }
+  void set_data(const std::vector<uint8_t> &data) { code_ = data; }
 
     void play(Ts... x) 
     {
-        std::vector<uint8_t> dt = this->data_.value(x...);
+        std::vector<uint8_t> dt = this->code_.value(x...);
 
         this->parent_->pump(dt);
     }
 
-    TEMPLATABLE_VALUE(std::vector<uint8_t>, data);
+    TEMPLATABLE_VALUE(std::vector<uint8_t>, code);
 
     protected:
     MyComponent *parent_;
