44ad9880db72b785f18e0eeff4ed42f3eca8ea89
wert95 <wert.95@hotmail.com>
2023-10-24 08:52:28 +0300

    Update component.h

--- components/water_quality/component.h
+++ components/water_quality/component.h
@@ -46,11 +46,10 @@ void loop() override;
 void pump_calibration(const std::vector<uint8_t> &x,
                       const std::vector<uint8_t> &y) 
 { 
-    for(int i = 0; i < 8; i++)
-    {
-        this->Pump_Calib_X1[i]= x[i];
-        this->Pump_Calib_Y1[i]= y[i];
-    }
+    this->Pump_Calib_X1 = x;
+    this->Pump_Calib_Y1 = y;
+    Pump_Calib_X1[8] = "\0";
+    Pump_Calib_Y1[8] = "\0";
     // this->Pump_Calib_X2 = x[1];
     // this->Pump_Calib_Y2 = y[1];
     // this->Pump_Calib_X3 = x[2];
