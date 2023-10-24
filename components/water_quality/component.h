ae40df27bc90b641ce289f4254656dccf5cd58ed
wert95 <wert.95@hotmail.com>
2023-10-24 23:11:22 +0300

    Update component.h

--- components/water_quality/component.h
+++ components/water_quality/component.h
@@ -52,7 +52,7 @@ void pump_calibration(const std::vector<std::vector<uint8_t>> &x) //uint8_t x[][
     this->Pump_Calib = x;
 }
 
-void pump_type(std::vector<uint8_t> &t)
+void pump_type(const std::vector<uint8_t> &t)
 {
     for (int i = 0; i < 8; i++)
     {
