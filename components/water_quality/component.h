1f85272569072549e6dcd6129b8639b20bf7da69
wert95 <wert.95@hotmail.com>
2023-10-24 09:03:36 +0300

    Update component.h

--- components/water_quality/component.h
+++ components/water_quality/component.h
@@ -74,7 +74,7 @@ void AnIn_Status(sensor::Sensor *a)
 }
 
 
-uint8_t Pump_Calib_X1[8]{0};
+uint8_t* Pump_Calib_X1[8]{0};
 // std::vector<uint8_t> Pump_Calib_X1{0};
 std::vector<uint8_t> Pump_Calib_Y1{0};
 std::vector<uint8_t> Pump_Calib_X2{};
