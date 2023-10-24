0d8fe48a8000d243a0f3ea86ea6dd4086c8dd80f
wert95 <wert.95@hotmail.com>
2023-10-25 01:39:07 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -186,9 +186,10 @@ async def to_code(config):
             
     con = config[CONF_PUMP2][0]
     type.append(con[CONF_PUMP_TYPE])
-    # if con[CONF_PUMP_TYPE] != 0:
-    #     for conf in con[CONF_PUMP_CALIBRATION]:
-    #         calib.append([conf[CONF_X]],[conf[CONF_Y]])
+    if con[CONF_PUMP_TYPE] != 0:
+        for conf in con[CONF_PUMP_CALIBRATION]:
+            calib.append([conf[CONF_X]])
+            calib.append([conf[CONF_Y]])
         
     con = config[CONF_PUMP3][0]
     type.append(con[CONF_PUMP_TYPE])
