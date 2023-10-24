a17ce450975426633f3bca5ead883304da2b0d02
wert95 <wert.95@hotmail.com>
2023-10-24 18:28:24 +0300

    Update component.cpp

--- components/water_quality/component.cpp
+++ components/water_quality/component.cpp
@@ -26,7 +26,7 @@ void MyComponent::loop()
     // ESP_LOGD(TAG,"%d", sizeof(Pump_Calib_Y5));
     // ESP_LOGD(TAG,"%d", sizeof(Pump_Calib_X6));
     // ESP_LOGD(TAG,"%d", sizeof(Pump_Calib_Y6));
-    // ESP_LOGD(TAG,"%d", sizeof(Pump_Calib));
+    ESP_LOGD(TAG,"%d", sizeof(Pump_Calib));
 
     // for (size_t i = 0; i < sizeof(Pump_Calib_X1) / sizeof(Pump_Calib_X1[0]); i++)
     // {
@@ -40,9 +40,9 @@ void MyComponent::loop()
     //     // ESP_LOGD(TAG,"y4[%d] = %d", i, Pump_Calib_Y4[i]);
     // }
 
-    for (size_t i = 0; i < sizeof(Pump_Calib) / sizeof(Pump_Calib[0][0]); i++)
+    for (size_t i = 0; i < sizeof(Pump_Calib); i++)
     {
-        for (size_t j = 0; j < sizeof(Pump_Calib) / sizeof(Pump_Calib[0][0]); j++)
+        for (size_t j = 0; j < sizeof(Pump_Calib); j++)
         {
             ESP_LOGD(TAG,"%d = %d", i, Pump_Calib[i][j]);
         }
