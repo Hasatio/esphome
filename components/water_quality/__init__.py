155e22504d69b2a789f4251bc7b85defcdfc266f
wert95 <wert.95@hotmail.com>
2023-10-25 22:49:03 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -214,8 +214,8 @@ async def pump_mode_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg, paren)
 
     mode = config[CONF_PUMP_MODE]
-    template_ = await cg.templatable(mode, args, cg.uint8)
-    # template_ = await cg.templatable(mode, args, cg.std_vector.template(cg.uint8))
+    # template_ = await cg.templatable(mode, args, cg.uint8)
+    template_ = await cg.templatable(mode, args, cg.std_vector.template(cg.uint8))
     cg.add(var.set_mode(template_))
     # cg.add(var.set_mode(mode))
 
