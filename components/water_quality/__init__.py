2fed2dbe40f86561106fffbe437116cdf78344e3
wert95 <wert.95@hotmail.com>
2023-10-26 00:53:45 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -174,12 +174,12 @@ PUMP_MODE_ACTION_SCHEMA = cv.All(
     {
         cv.GenerateID(): cv.use_id(MyComponent),
         cv.Required(CONF_PUMP_MODE): (
-            # cv.All(
+            cv.All(
                 # [cv.Any(cv.uint8_t)],
                 [cv.ensure_list(cv.uint8_t)],
                 # cv.templatable(cv.uint8_t),
                 # cv.Length(min=0, max=3),
-            # )
+            )
         ),
         # cv.Required(CONF_PUMP_MODE):
         #     cv.templatable(cv.int_range()),
