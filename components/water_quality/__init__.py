0af05afea15feb992119614f7f9316ab1319618d
wert95 <wert.95@hotmail.com>
2023-10-25 22:17:07 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -185,7 +185,6 @@ PUMP_MODE_ACTION_SCHEMA = cv.All(
 
 PUMP_DOSE_ACTION_SCHEMA = cv.All(
     {
-        # cv.Required(CONF_ID): cv.use_id(MyComponent),
         cv.GenerateID(): cv.use_id(MyComponent),
         # cv.Required(CONF_DOSE): cv.templatable(
         #     cv.int_range()
@@ -215,7 +214,7 @@ async def pump_mode_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg, paren)
 
     mode = config[CONF_PUMP_MODE]
-    cg.add(var.pump_mode(mode))
+    cg.add(var.set_mode(mode))
 
     return var
 
