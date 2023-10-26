427f213ae052c7cea27ca24c3d5296d570b35669
wert95 <wert.95@hotmail.com>
2023-10-26 04:12:39 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -200,7 +200,7 @@ PUMP_DOSE_ACTION_SCHEMA = cv.All(
         # ),
         cv.Required(CONF_PUMP_DOSE): cv.All(
             
-            [cv.ensure_list(cv.uint8_t)],
+            cv.ensure_list(cv.uint8_t),
             cv.Length(min=0, max=3),
         ),
     }
