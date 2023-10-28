4aec453bcff43c1a2f996353d7bd4dbd30c99b89
wert95 <wert.95@hotmail.com>
2023-10-28 09:59:22 +0300

    Update template.h

--- components/water_quality/template.h
+++ components/water_quality/template.h
@@ -1,3 +1,14 @@
+#pragma once
+
+#include "esphome.h"
+#include "esphome/core/log.h"
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/core/automation.h"
+// #include <Wire.h>
+#include <array>
+#include <vector>
+#include <iterator>
 
 namespace esphome {
 namespace water_quality {
