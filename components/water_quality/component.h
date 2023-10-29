c25dfc0006cd2eb86a5c54955ad4f67ae3788991
wert95 <wert.95@hotmail.com>
2023-10-29 23:41:40 +0300

    Update component.h

--- components/water_quality/component.h
+++ components/water_quality/component.h
@@ -1,29 +1,8 @@
-#pragma once
-
-#include "esphome.h"
-#include "esphome/core/log.h"
-#include "esphome/core/component.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/i2c/i2c.h"
-#include "esphome/core/automation.h"
-#include <array>
-#include <vector>
-#include <iterator>
-#include <Wire.h>
-#include <Adafruit_ADS1X15.h>
-#include <Adafruit_MCP23X17.h>
-#include "Adafruit_MCP23X08.h"
-#include <Adafruit_PWMServoDriver.h>
 
 namespace esphome {
 namespace water_quality {
 
 static const char *const TAG = "component";
-
-
-void settings()
-{
-}
 
 class MyComponent
 {
