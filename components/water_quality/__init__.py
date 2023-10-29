609587c2e3740309d5f80e4a8b2a1caa8918b030
wert95 <wert.95@hotmail.com>
2023-10-29 09:36:11 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import i2c
 from esphome import automation
 from esphome.const import (
     CONF_ID,
@@ -76,7 +77,7 @@ PUMP_TYPE_SCHEMA = cv.typed_schema(
 )
 
 component_ns = cg.esphome_ns.namespace("water_quality")
-MyComponent = component_ns.class_("MyComponent", cg.Component)
+MyComponent = component_ns.class_("MyComponent", cg.Component, i2c.I2CDevice)
 
                     
 CONFIG_SCHEMA = cv.All(
@@ -149,13 +150,15 @@ CONFIG_SCHEMA = cv.All(
             ),
         }
     )
-    .extend(cv.COMPONENT_SCHEMA),
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x70))
 )
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
     
     type = []
     calib = []
