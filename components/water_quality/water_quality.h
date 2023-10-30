407ce132b99d8570651d8cf06d9fc82e497bcae4
wert95 <wert.95@hotmail.com>
2023-10-30 03:28:50 +0300

    Update water_quality.h

--- components/water_quality/water_quality.h
+++ components/water_quality/water_quality.h
@@ -488,7 +488,7 @@ uint8_t dose, circ;
 std::vector<uint8_t> Pump_Mode{0,0,0,0,0,0};
 std::vector<uint8_t> Pump_Dose{0,0,0,0,0,0};
 std::vector<uint16_t> Pump_Circulation{0,0,0,0,0,0};
-std::vector<std::vector<uint16_t>> Pump_Total(6, std::vector<uint16_t>(2, 0));
+std::vector<std::vector<uint16_t>> Pump_Total(2, std::vector<uint16_t>(2, 0));
 std::vector<bool> Pump_Reset{0,0,0,0,0,0};
 std::vector<bool> Servo_Mode{0,0,0,0,0,0,0,0};
 std::vector<uint8_t> Servo_Position{0,0,0,0,0,0,0,0};
