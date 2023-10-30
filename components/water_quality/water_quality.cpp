1636d85553826c40e3c7fa33286e380902cda25d
wert95 <wert.95@hotmail.com>
2023-10-31 02:44:11 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -5,6 +5,7 @@ namespace water_quality {
 
 void MyComponent::loop() 
 {
+    pump_total();
     // delay(1000);
 }
 
@@ -13,7 +14,6 @@ void MyComponent::update()
     ads1115();
     mcp23008();
     pca9685();
-    pump_total();
     // sensor();
 }
 
