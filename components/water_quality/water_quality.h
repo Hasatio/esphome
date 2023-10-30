98fbe883a022c4f2b0eec2463edafcc426d588ac
wert95 <wert.95@hotmail.com>
2023-10-30 07:53:49 +0300

    Update water_quality.h

--- components/water_quality/water_quality.h
+++ components/water_quality/water_quality.h
@@ -405,7 +405,7 @@ void pump_mode(std::vector<uint8_t> &pmode)
     for (size_t i = 0; i < (dose + circ); i++)
     {
         if (pmode[i] == 1)
-        this->pump_total(Pump_Dose);
+        this->pump_total(this->Pump_Dose);
         ESP_LOGD(TAG,"Pump_Mode[%d] = %d", i, pmode[i]);
     }
 
