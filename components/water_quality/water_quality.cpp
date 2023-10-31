e36a75f6b855a6cfe1408a5f315a06b069a79087
wert95 <wert.95@hotmail.com>
2023-10-31 11:35:52 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -8,14 +8,5 @@ void MyComponent::loop()
     // delay(1000);
 }
 
-void MyComponent::update()
-{
-    ads1115();
-    mcp23008();
-    pca9685();
-    // pump_total();
-    // sensor();
-}
-
 }  // namespace water_quality
 }  // namespace esphome
