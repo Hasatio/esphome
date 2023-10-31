750b026b8947ca4bc110cbe68e0f644407a469ff
wert95 <wert.95@hotmail.com>
2023-10-31 09:35:40 +0300

    Update water_quality.h

--- components/water_quality/water_quality.h
+++ components/water_quality/water_quality.h
@@ -333,7 +333,7 @@ void dump_config() override
     for (size_t i = 0; i < Pump_Type.size(); i++)
     {
         ESP_LOGI(TAG,"Pump_Type[%d] = %d", i, Pump_Type[i]);
-        // ESP_LOGI(TAG,"Pump_Total[%d] = %d.%d", i, Pump_Total[i][0], Pump_Total[i][1]);
+        ESP_LOGI(TAG,"Pump_Total[%d] = %d.%d", i, Pump_Total[i][0], Pump_Total[i][1]);
     }
 
     for (size_t i = 0; i < AnInLvl_ResMin.size(); i++)
