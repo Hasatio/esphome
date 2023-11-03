78e91e4f6c5cb131dfeab5c658430df8d6e35679
wert95 <wert.95@hotmail.com>
2023-11-03 19:49:22 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -43,7 +43,7 @@ PUMP_TYPE_CIRCULATION = 2
 
 PUMP_CALIBRATION_SCHEMA = cv.Schema(
     {
-        cv.Required(CONF_PUMP_CALIB_GAIN): cv.float
+        cv.Required(CONF_PUMP_CALIB_GAIN): cv.float_
     }
 )
                     
