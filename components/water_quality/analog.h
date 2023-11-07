7770c369f4c6f8229ccefbb1675d02659533faa5
wert95 <wert.95@hotmail.com>
2023-11-07 20:16:47 +0300

    Update analog.h

--- components/water_quality/analog.h
+++ components/water_quality/analog.h
@@ -53,8 +53,8 @@ uint8_t AnInEC_Ch, AnInEC_Type, AnInPH_Ch, AnInPH_Type;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    AnInEC_Type == 1? DFRobot_EC ec;
-    AnInEC_Type == 10? DFRobot_EC10 ec;
+    AnInEC_Type == 1? DFRobot_EC ec : "";
+    AnInEC_Type == 10? DFRobot_EC10 ec : "";
 
     DFRobot_PH ph;
 
