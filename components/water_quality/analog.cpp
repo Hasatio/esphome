9a395f091edccaac4b7a3d80950935f13b775ee6
wert95 <wert.95@hotmail.com>
2023-11-09 13:04:27 +0300

    Update analog.cpp

--- components/water_quality/analog.cpp
+++ components/water_quality/analog.cpp
@@ -101,18 +101,18 @@ void Analog::ads1115()
 
 void MyComponent::level_res(const std::vector<uint16_t> &rmin, const std::vector<uint16_t> &rmax)
 {
-    ana.AnInLvl_ResMin = rmin;
-    ana.AnInLvl_ResMax = rmax;
+    this->ana.AnInLvl_ResMin = rmin;
+    this->ana.AnInLvl_ResMax = rmax;
 }
 void MyComponent::ec(const uint8_t ch, const uint8_t type)
 {
-    ana.AnInEC_Ch = ch;
-    ana.AnInEC_Type = type;
+    this->ana.AnInEC_Ch = ch;
+    this->ana.AnInEC_Type = type;
 }
 void MyComponent::ph(const uint8_t ch, const uint8_t type)
 {
-    ana.AnInPH_Ch = ch;
-    ana.AnInPH_Type = type;
+    this->ana.AnInPH_Ch = ch;
+    this->ana.AnInPH_Type = type;
 }
 
 void Analog::Analog_Input_Driver()
