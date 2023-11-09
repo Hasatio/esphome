c60cc27361925de696b3d75f9a6c936785e68fab
wert95 <wert.95@hotmail.com>
2023-11-09 13:38:50 +0300

    Update analog.cpp

--- components/water_quality/analog.cpp
+++ components/water_quality/analog.cpp
@@ -3,7 +3,7 @@
 
     Mux muxe;
     Analog ana;
-    
+
 namespace esphome {
 namespace water_quality {
 
@@ -102,8 +102,8 @@ void Analog::ads1115()
 
 void MyComponent::level_res(const std::vector<uint16_t> &rmin, const std::vector<uint16_t> &rmax)
 {
-    ana.AnInLvl_ResMin = rmin;
-    ana.AnInLvl_ResMax = rmax;
+    ana->AnInLvl_ResMin = rmin;
+    this->ana->AnInLvl_ResMax = rmax;
 }
 void MyComponent::ec(const uint8_t ch, const uint8_t type)
 {
