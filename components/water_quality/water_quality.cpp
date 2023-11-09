934b9a72471360244d7576573445b61d17500015
wert95 <wert.95@hotmail.com>
2023-11-09 12:49:10 +0300

    .

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -20,7 +20,56 @@ void MyComponent::setup()
     pca9685_set();
 }
 
+void MyComponent::dump_config() override
+{
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  TCA9548
 
+    // Wire.begin(SDA,SCL,frq);
+
+    // for (uint8_t t=0; t<8; t++) 
+    // {
+    //   mux.tcaselect(t);
+    //   ESP_LOGI(TAG,"TCA Port %d", t);
+
+    //   for (uint8_t addr = 0; addr<=127; addr++) 
+    //   {
+    //     if (addr == TCA9548_ADDRESS) continue;
+
+    //     Wire.beginTransmission(addr);
+    //     if (!Wire.endTransmission()) 
+    //     {
+    //       ESP_LOGI(TAG,"Found I2C 0x%x",addr);
+    //     }
+    //   }
+    // }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    ESP_LOGI(TAG,"Pump_dose = %d", pump.dose);
+    ESP_LOGI(TAG,"Pump_circ = %d", pump.circ);
+
+    for (size_t i = 0; i < pump.Pump_Type.size(); i++)
+    {
+        ESP_LOGI(TAG,"Pump_Calib_Gain[%d] = %.2f", i, pump.Pump_Calib_Gain[i]);
+    }
+
+    for (size_t i = 0; i < pump.Pump_Type.size(); i++)
+    {
+        ESP_LOGI(TAG,"Pump_Type[%d] = %d", i, pump.Pump_Type[i]);
+        ESP_LOGI(TAG,"Pump_Total[%d] = %d.%d", i, pump.Pump_Total[i][0], pump.Pump_Total[i][1]);
+    }
+
+    // for (size_t i = 0; i < an.AnInLvl_ResMin.size(); i++)
+    // {
+    //     ESP_LOGI(TAG,"ResMin[%d] = %d", i, an.AnInLvl_ResMin[i]);
+    //     ESP_LOGI(TAG,"ResMax[%d] = %d", i, an.AnInLvl_ResMax[i]);
+    // }
+
+    // ESP_LOGI(TAG,"EC_ch = %d", an.AnInEC_Ch);
+    // ESP_LOGI(TAG,"EC_type = %d", an.AnInEC_Type);
+    // ESP_LOGI(TAG,"PH_ch = %d", an.AnInPH_Ch);
+    // ESP_LOGI(TAG,"PH_type = %d", an.AnInPH_Type);
+}
 
 void MyComponent::loop() 
 {
