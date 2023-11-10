83fef75947067953ec2f98e6951718f7a6828abf
wert95 <wert.95@hotmail.com>
2023-11-10 13:19:09 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -229,9 +229,9 @@ void MyComponent::sensor()
     {
         for (size_t i = 0; i < 2; i++)
         if (i == 0)
-        sprintf(gen + strlen(gen), "%.2f", an.AnGen[i]);
-        else
-        sprintf(gen + strlen(gen), ",%.2f", an.AnGen[i]);
+        // sprintf(gen + strlen(gen), "%.2f", an.AnGen[i]);
+        // else
+        // sprintf(gen + strlen(gen), ",%.2f", an.AnGen[i]);
     } 
     this->AnInGen_Val_->publish_state(gen);
 }
