6be7d797189fe8ea2c70def7a3e7c3ba5b146591
wert95 <wert.95@hotmail.com>
2023-11-10 12:06:01 +0300

    Update water_quality.h

--- components/water_quality/water_quality.h
+++ components/water_quality/water_quality.h
@@ -12,6 +12,7 @@
 #include "esphome/core/log.h"
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/automation.h"
 #include <array>
