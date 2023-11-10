68022dc7286cde0ac41469094688eaa5bf36b3e5
wert95 <wert.95@hotmail.com>
2023-11-10 20:26:46 +0300

    Update analog.cpp

--- components/water_quality/analog.cpp
+++ components/water_quality/analog.cpp
@@ -6,7 +6,7 @@ namespace esphome {
 namespace water_quality {
 
     Mux muxe;
-    Analog an;
+    Analog ana;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  ADS1115
@@ -87,13 +87,13 @@ void MyComponent::ads1115()
     // muxe.tcaselect(0);
     for(size_t i = 0; i < 4; i++)
     {
-        an.adc[i] = ads1.readADC_SingleEnded(i%4);
-        an.volts[i] = ads1.computeVolts(an.adc[i]);
+        ana.adc[i] = ads1.readADC_SingleEnded(i%4);
+        ana.volts[i] = ads1.computeVolts(ana.adc[i]);
         // ESP_LOGD(TAG,"ads%d = %f", i+1, volts[i]);
     }
     for(size_t i = 4; i < 8; i++){
-        an.adc[i] = ads2.readADC_SingleEnded(i%4);
-        an.volts[i] = ads2.computeVolts(an.adc[i]);
+        ana.adc[i] = ads2.readADC_SingleEnded(i%4);
+        ana.volts[i] = ads2.computeVolts(ana.adc[i]);
         // ESP_LOGD(TAG,"ads%d = %f", i+1, volts[i]);
     }
 }
