5dced8dc89a9c3ae0590aaeb81bd952ed80e06f5
wert95 <wert.95@hotmail.com>
2023-11-10 20:33:36 +0300

    Update analog.cpp

--- components/water_quality/analog.cpp
+++ components/water_quality/analog.cpp
@@ -31,111 +31,111 @@ void Analog::Analog_Input_Driver()
 }
 
 bool calibrationIsRunning = false;
-void Analog::ec_ph()
-{
-	unsigned long now = millis();
-	if (now - last[0] >= intervals[0]) //1000ms interval
-	{
-		last[0] = now;
-		if (calibrationIsRunning)
-		{
-			Serial.println(F("[main]...>>>>>> calibration is running, to exit send exitph or exitec through serial <<<<<<"));
-			//water temperature
-			temperature = getWaterTemperature();
-			//EC
-			// ecVoltage = ads2.readADC_SingleEnded(3) / 10;
-			Serial.print(F("[EC Voltage]... ecVoltage: "));
-			Serial.println(ecVoltage);
-			EC = ec.readEC(ecVoltage, temperature); // convert voltage to EC with temperature compensation
-			Serial.print(F("[EC Read]... EC: "));
-			Serial.print(EC);
-			Serial.println(F("ms/cm"));
-			//pH
-			// phVoltage = ads2.readADC_SingleEnded(1) / 10;
-			Serial.print(F("[pH Voltage]... phVoltage: "));
-			Serial.println(phVoltage);
-			PH = ph.readPH(phVoltage, temperature);
-			Serial.print(F("[pH Read]... pH: "));
-			Serial.println(PH);
-		}
+// void Analog::ec_ph()
+// {
+// 	unsigned long now = millis();
+// 	if (now - last[0] >= intervals[0]) //1000ms interval
+// 	{
+// 		last[0] = now;
+// 		if (calibrationIsRunning)
+// 		{
+// 			Serial.println(F("[main]...>>>>>> calibration is running, to exit send exitph or exitec through serial <<<<<<"));
+// 			//water temperature
+// 			temperature = getWaterTemperature();
+// 			//EC
+// 			// ecVoltage = ads2.readADC_SingleEnded(3) / 10;
+// 			Serial.print(F("[EC Voltage]... ecVoltage: "));
+// 			Serial.println(ecVoltage);
+// 			EC = ec.readEC(ecVoltage, temperature); // convert voltage to EC with temperature compensation
+// 			Serial.print(F("[EC Read]... EC: "));
+// 			Serial.print(EC);
+// 			Serial.println(F("ms/cm"));
+// 			//pH
+// 			// phVoltage = ads2.readADC_SingleEnded(1) / 10;
+// 			Serial.print(F("[pH Voltage]... phVoltage: "));
+// 			Serial.println(phVoltage);
+// 			PH = ph.readPH(phVoltage, temperature);
+// 			Serial.print(F("[pH Read]... pH: "));
+// 			Serial.println(PH);
+// 		}
 
-		if (readSerial(cmd))
-		{
-			strupr(cmd);
-			if (calibrationIsRunning || strstr(cmd, "PH") || strstr(cmd, "EC"))
-			{
-				calibrationIsRunning = true;
-				Serial.println(F("[]... >>>>>calibration is now running PH and EC are both reading, if you want to stop this process enter EXITPH or EXITEC in Serial Monitor<<<<<"));
-				if (strstr(cmd, "PH"))
-				{
-					ph.calibration(phVoltage, temperature, cmd); //PH calibration process by Serail CMD
-				}
-				if (strstr(cmd, "EC"))
-				{
-					ec.calibration(ecVoltage, temperature, cmd); //EC calibration process by Serail CMD
-				}
-			}
-			if (strstr(cmd, "EXITPH") || strstr(cmd, "EXITEC"))
-			{
-				calibrationIsRunning = false;
-			}
-		}
-	}
-	if (now - last[3] >= intervals[3]) //5000ms interval
-	{
-		last[3] = now; 
-		if (!calibrationIsRunning)
-		{
-			temperature = getWaterTemperature(); // read your temperature sensor to execute temperature compensation
-			Serial.print("temperature:");
-			Serial.print(temperature, 1);
-			Serial.println("^C");
+// 		if (readSerial(cmd))
+// 		{
+// 			strupr(cmd);
+// 			if (calibrationIsRunning || strstr(cmd, "PH") || strstr(cmd, "EC"))
+// 			{
+// 				calibrationIsRunning = true;
+// 				Serial.println(F("[]... >>>>>calibration is now running PH and EC are both reading, if you want to stop this process enter EXITPH or EXITEC in Serial Monitor<<<<<"));
+// 				if (strstr(cmd, "PH"))
+// 				{
+// 					ph.calibration(phVoltage, temperature, cmd); //PH calibration process by Serail CMD
+// 				}
+// 				if (strstr(cmd, "EC"))
+// 				{
+// 					ec.calibration(ecVoltage, temperature, cmd); //EC calibration process by Serail CMD
+// 				}
+// 			}
+// 			if (strstr(cmd, "EXITPH") || strstr(cmd, "EXITEC"))
+// 			{
+// 				calibrationIsRunning = false;
+// 			}
+// 		}
+// 	}
+// 	if (now - last[3] >= intervals[3]) //5000ms interval
+// 	{
+// 		last[3] = now; 
+// 		if (!calibrationIsRunning)
+// 		{
+// 			temperature = getWaterTemperature(); // read your temperature sensor to execute temperature compensation
+// 			Serial.print("temperature:");
+// 			Serial.print(temperature, 1);
+// 			Serial.println("^C");
 
-			// ecVoltage = ads2.readADC_SingleEnded(3) / 10;
-			Serial.print("ecVoltage:");
-			Serial.println(ecVoltage, 4);
+// 			// ecVoltage = ads2.readADC_SingleEnded(3) / 10;
+// 			Serial.print("ecVoltage:");
+// 			Serial.println(ecVoltage, 4);
 
-			EC = ec.readEC(ecVoltage, temperature); // convert voltage to EC with temperature compensation
-			Serial.print("EC:");
-			Serial.print(EC, 4);
-			Serial.println("ms/cm");
+// 			EC = ec.readEC(ecVoltage, temperature); // convert voltage to EC with temperature compensation
+// 			Serial.print("EC:");
+// 			Serial.print(EC, 4);
+// 			Serial.println("ms/cm");
 
-			// phVoltage = ads2.readADC_SingleEnded(1) / 10; // read the voltage
-			Serial.print("phVoltage:");
-			Serial.println(phVoltage, 4);
-			PH = ph.readPH(phVoltage, temperature); // convert voltage to pH with temperature compensation
-			Serial.print("pH:");
-			Serial.println(PH, 4);
-		}
-	}
-}
+// 			// phVoltage = ads2.readADC_SingleEnded(1) / 10; // read the voltage
+// 			Serial.print("phVoltage:");
+// 			Serial.println(phVoltage, 4);
+// 			PH = ph.readPH(phVoltage, temperature); // convert voltage to pH with temperature compensation
+// 			Serial.print("pH:");
+// 			Serial.println(PH, 4);
+// 		}
+// 	}
+// }
 
-void Analog::ec_ph2()
-{
-    if(millis()-timepoint>1000U)                             //time interval: 1s
-    {
-        timepoint = millis();
-        //temperature = readTemperature();                   // read your temperature sensor to execute temperature compensation
-        // voltagePH = analogRead(PH_PIN)/1024.0*5000;          // read the ph voltage
-        // PH    = ph.readPH(voltagePH,WT);       // convert voltage to pH with temperature compensation
-        Serial.print("pH:");
-        Serial.print(PH,2);
-        // voltageEC = analogRead(EC_PIN)/1024.0*5000;
-        // EC    = ec.readEC(voltageEC,WT);       // convert voltage to EC with temperature compensation
-        Serial.print(", EC:");
-        Serial.print(EC,2);
-        Serial.println("ms/cm");
-    }
-    if(readSerial(cmd)){
-        strupr(cmd);
-        if(strstr(cmd,"PH")){
-            ph.calibration(voltagePH,WT,cmd);       //PH calibration process by Serail CMD
-        }
-        if(strstr(cmd,"EC")){
-            ec.calibration(voltageEC,WT,cmd);       //EC calibration process by Serail CMD
-        }
-    }
-}
+// void Analog::ec_ph2()
+// {
+//     if(millis()-timepoint>1000U)                             //time interval: 1s
+//     {
+//         timepoint = millis();
+//         //temperature = readTemperature();                   // read your temperature sensor to execute temperature compensation
+//         // voltagePH = analogRead(PH_PIN)/1024.0*5000;          // read the ph voltage
+//         // PH    = ph.readPH(voltagePH,WT);       // convert voltage to pH with temperature compensation
+//         Serial.print("pH:");
+//         Serial.print(PH,2);
+//         // voltageEC = analogRead(EC_PIN)/1024.0*5000;
+//         // EC    = ec.readEC(voltageEC,WT);       // convert voltage to EC with temperature compensation
+//         Serial.print(", EC:");
+//         Serial.print(EC,2);
+//         Serial.println("ms/cm");
+//     }
+//     if(readSerial(cmd)){
+//         strupr(cmd);
+//         if(strstr(cmd,"PH")){
+//             ph.calibration(voltagePH,WT,cmd);       //PH calibration process by Serail CMD
+//         }
+//         if(strstr(cmd,"EC")){
+//             ec.calibration(voltageEC,WT,cmd);       //EC calibration process by Serail CMD
+//         }
+//     }
+// }
 
 float Analog::getWaterTemperature()
 {
