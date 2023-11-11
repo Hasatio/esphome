0b50d8834ee62b811a13427416120d9ba63d585d
wert95 <wert.95@hotmail.com>
2023-11-11 03:25:17 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -116,7 +116,7 @@ void MyComponent::dump_config()
     if (this->is_failed())
     ESP_LOGE(TAG, "Communication with ADS1115 failed!");
     
-        ESP_LOGD(TAG,"ads = %f", volts[1]);
+        ESP_LOGD(TAG,"ads = %f", volts[1]*10);
         ESP_LOGD(TAG,"ads1 = %f", ads1.readADC_SingleEnded(1)*10);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  TCA9548
