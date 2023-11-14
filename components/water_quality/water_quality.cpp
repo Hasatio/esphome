ee355cb718fd9c5954f7c44bf15f472273187fe7
wert95 <wert.95@hotmail.com>
2023-11-15 02:29:44 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -25,17 +25,15 @@ static unsigned long timepoint = millis();
 
 void MyComponent::ADS1115_Setup()
 {
-    bool status1 = ads1.begin(ADS1X15_ADDRESS1);
-    if (!status1)
+    if (ads1.begin(ADS1X15_ADDRESS1))
     {
       ESP_LOGE(TAG,"Failed to initialize ADS1115_1.");
-    //   while (1);
+      while (1);
     }
-    bool status2 = ads2.begin(ADS1X15_ADDRESS2);
-    if (!status2)
+    if (ads2.begin(ADS1X15_ADDRESS2))
     {
       ESP_LOGE(TAG,"Failed to initialize ADS1115_2.");
-    //   while (1);
+      while (1);
     }
 
     // The ADC input range (or gain) can be changed via the following
