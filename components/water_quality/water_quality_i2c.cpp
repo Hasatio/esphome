98894e3fbf5e2a3adc9c8b638de4b6235fc7cb11
wert95 <wert.95@hotmail.com>
2023-11-15 02:53:12 +0300

    Update water_quality_i2c.cpp

--- components/water_quality/water_quality_i2c.cpp
+++ components/water_quality/water_quality_i2c.cpp
@@ -56,50 +56,6 @@ void ADS1115_Setup()
         // while (1);
     else
         ESP_LOGI(TAG,"Successfulled to initialize ADS1115_2.");
-
-    // The ADC input range (or gain) can be changed via the following
-    // functions, but be careful never to exceed VDD +0.3V max, or to
-    // exceed the upper and lower limits if you adjust the input range!
-    // Setting these values incorrectly may destroy your ADC!
-    
-    //                                          ADS1015          ADS1115
-    //                                          -------          -------
-    // GAIN_TWOTHIRDS  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-    // GAIN_ONE        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-    // GAIN_TWO        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-    // GAIN_FOUR       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
-    // GAIN_EIGHT      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-    // GAIN_SIXTEEN    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-    ads1.setGain(GAIN_TWOTHIRDS);
-    ads2.setGain(GAIN_TWOTHIRDS);
-    
-    // RATE_ADS1115_8SPS (0x0000)   ///< 8 samples per second
-    // RATE_ADS1115_16SPS (0x0020)  ///< 16 samples per second
-    // RATE_ADS1115_32SPS (0x0040)  ///< 32 samples per second
-    // RATE_ADS1115_64SPS (0x0060)  ///< 64 samples per second
-    // RATE_ADS1115_128SPS (0x0080) ///< 128 samples per second (default)
-    // RATE_ADS1115_250SPS (0x00A0) ///< 250 samples per second
-    // RATE_ADS1115_475SPS (0x00C0) ///< 475 samples per second
-    // RATE_ADS1115_860SPS (0x00E0) ///< 860 samples per second
-    ads1.setDataRate(RATE_ADS1115_860SPS);
-    ads2.setDataRate(RATE_ADS1115_860SPS);
-    
-    // ADS1X15_REG_CONFIG_MUX_DIFF_0_1 (0x0000) ///< Differential P = AIN0, N = AIN1 (default)
-    // ADS1X15_REG_CONFIG_MUX_DIFF_0_3 (0x1000) ///< Differential P = AIN0, N = AIN3
-    // ADS1X15_REG_CONFIG_MUX_DIFF_1_3 (0x2000) ///< Differential P = AIN1, N = AIN3
-    // ADS1X15_REG_CONFIG_MUX_DIFF_2_3 (0x3000) ///< Differential P = AIN2, N = AIN3
-    // ADS1X15_REG_CONFIG_MUX_SINGLE_0 (0x4000) ///< Single-ended AIN0
-    // ADS1X15_REG_CONFIG_MUX_SINGLE_1 (0x5000) ///< Single-ended AIN1
-    // ADS1X15_REG_CONFIG_MUX_SINGLE_2 (0x6000) ///< Single-ended AIN2
-    // ADS1X15_REG_CONFIG_MUX_SINGLE_3 (0x7000) ///< Single-ended AIN3
-    // ads1.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1, true);
-    // ads2.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1, true);
-
-    // AnInEC_Type == 1? EC():EC10();
-    // // AnInEC_Type == 10? EC10();
-
-    // ec.begin();
-    // ph.begin();
 }
 void ADS1115_Driver()
 {
