79a6ae2e54c2dd51f7cbdf7b9884b2ed6957af03
wert95 <wert.95@hotmail.com>
2023-11-15 02:23:10 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -26,12 +26,12 @@ void MyComponent::ADS1115_Setup()
     if (!ads1.begin(ADS1X15_ADDRESS1))
     {
       ESP_LOGE(TAG,"Failed to initialize ADS1115_1.");
-    //   while (1);
+      while (1);
     }
     if (!ads2.begin(ADS1X15_ADDRESS2))
     {
       ESP_LOGE(TAG,"Failed to initialize ADS1115_2.");
-    //   while (1);
+      while (1);
     }
 
     // The ADC input range (or gain) can be changed via the following
@@ -109,7 +109,7 @@ void MyComponent::dump_config()
     if (this->is_failed())
     ESP_LOGE(TAG, "Communication failed!");
     if (!this->is_failed())
-    ESP_LOGE(TAG, "Communication successful!");
+    ESP_LOGI(TAG, "Communication successful!");
     
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  TCA9548
