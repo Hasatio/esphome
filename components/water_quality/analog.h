815bdae60727bcc8007a7aa5396fdab53aefe3b6
wert95 <wert.95@hotmail.com>
2023-11-15 06:29:36 +0300

    Update analog.h

--- components/water_quality/analog.h
+++ components/water_quality/analog.h
@@ -55,6 +55,9 @@ bool readSerial(char result[]);
 void ec_ph();
 void ec_ph2();
 
+uint16_t AnInWT_Res = 1000; //temperature sensor model pt1000 and its resistance is 1k
+float AnInWT_Val, AnInVPow_Val, AnInLvl_Perc[2], AnInEC_Val, AnInPH_Val, AnInGen_Val[2];
+uint8_t AnInEC_Ch, AnInEC_Type, AnInPH_Ch, AnInPH_Type, AnInGen_Ch[2];
 std::vector<uint16_t> AnInLvl_ResMin{0,0};
 std::vector<uint16_t> AnInLvl_ResMax{0,0};
 float voltagePH, voltageEC, lastTemperature;
@@ -63,9 +66,6 @@ char cmd[10];
 float ecVoltage,phVoltage,temperature;
 
 protected:
-uint16_t AnInWT_Res = 1000; //temperature sensor model pt1000 and its resistance is 1k
-float AnInWT_Val, AnInVPow_Val, AnInLvl_Perc[2], AnInEC_Val, AnInPH_Val, AnInGen_Val[2];
-uint8_t AnInEC_Ch, AnInEC_Type, AnInPH_Ch, AnInPH_Type, AnInGen_Ch[2];
 
 };
 
