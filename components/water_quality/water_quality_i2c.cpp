fc9e9c54891f8ce01ba51ec26fc60b9cb7162aad
wert95 <wert.95@hotmail.com>
2023-11-16 22:30:24 +0300

    Update water_quality_i2c.cpp

--- components/water_quality/water_quality_i2c.cpp
+++ components/water_quality/water_quality_i2c.cpp
@@ -160,13 +160,13 @@ void MCP23008_Driver()
 
     for(size_t i = 0; i < 4; i++)
     {
-        digi.DigIn_Read[i] = mcp.digitalRead(i);
+        dig.DigIn_Read[i] = mcp.digitalRead(i);
         // ESP_LOGD(TAG,"dig input %d = %d", i, DigIn_Read[i]);
     }
 
     for(size_t i = 0; i < 4; i++)
     {
-        if (digi.DigOut_Status[i] == 1)
+        if (dig.DigOut_Status[i] == 1)
         {
             mcp.digitalWrite(i + 4, HIGH);
         }
