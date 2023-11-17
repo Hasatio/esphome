9354f125efad338b976e1d920d29150ce10a459e
wert95 <wert.95@hotmail.com>
2023-11-18 01:54:01 +0300

    Update water_quality.h

--- components/water_quality/water_quality.h
+++ components/water_quality/water_quality.h
@@ -32,38 +32,38 @@ float get_setup_priority() const override { return esphome::setup_priority::DATA
 
 uint16_t PwmFreq = 1000;
 
-    Adafruit_PWMServoDriver pwm;
+    // Adafruit_PWMServoDriver pwm;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  PCA9685
 void pca9685_set()
 {
-    // tcaselect(0);
-    Adafruit_PWMServoDriver(PCA9685_I2C_ADDRESS, Wire);
+    // // tcaselect(0);
+    // Adafruit_PWMServoDriver(PCA9685_I2C_ADDRESS, Wire);
     
-    if (!pwm.begin()) 
-    {
-      ESP_LOGE(TAG,"Failed to initialize PCA9685.");
-    //   while (1);
-    }
-    /*
-    * In theory the internal oscillator (clock) is 25MHz but it really isn't
-    * that precise. You can 'calibrate' this by tweaking this number until
-    * you get the PWM update frequency you're expecting!
-    * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-    * is used for calculating things like writeMicroseconds()
-    * Analog servos run at ~50 Hz updates, It is importaint to use an
-    * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-    * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-    *    the I2C PCA9685 chip you are setting the value for.
-    * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-    *    expected value (50Hz for most ESCs)
-    * Setting the value here is specific to each individual I2C PCA9685 chip and
-    * affects the calculations for the PWM update frequency. 
-    * Failure to correctly set the int.osc value will cause unexpected PWM results
-    */
-    pwm.setOscillatorFrequency(27000000);
-    pwm.setPWMFreq(PwmFreq);
+    // if (!pwm.begin()) 
+    // {
+    //   ESP_LOGE(TAG,"Failed to initialize PCA9685.");
+    // //   while (1);
+    // }
+    // /*
+    // * In theory the internal oscillator (clock) is 25MHz but it really isn't
+    // * that precise. You can 'calibrate' this by tweaking this number until
+    // * you get the PWM update frequency you're expecting!
+    // * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
+    // * is used for calculating things like writeMicroseconds()
+    // * Analog servos run at ~50 Hz updates, It is importaint to use an
+    // * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
+    // * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
+    // *    the I2C PCA9685 chip you are setting the value for.
+    // * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
+    // *    expected value (50Hz for most ESCs)
+    // * Setting the value here is specific to each individual I2C PCA9685 chip and
+    // * affects the calculations for the PWM update frequency. 
+    // * Failure to correctly set the int.osc value will cause unexpected PWM results
+    // */
+    // pwm.setOscillatorFrequency(27000000);
+    // pwm.setPWMFreq(PwmFreq);
 }
 void pca9685()
 {
