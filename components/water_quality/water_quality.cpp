555f24adcf3d9201b0982236b05e7859a201460d
wert95 <wert.95@hotmail.com>
2023-11-17 18:07:55 +0300

    .

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -14,7 +14,7 @@ static unsigned long timepoint = millis();
 
 void MyComponent::setup()
 {
-    ADS1115_Setup();
+    // ADS1115_Setup();
     // MCP23008_Setup();
 
     // PCA9685_Setup();
--- components/water_quality/water_quality_i2c.cpp
+++ components/water_quality/water_quality_i2c.cpp
@@ -102,12 +102,14 @@ void ADS1115_Driver(float analog_voltage[])
 {
     for(size_t i = 0; i < 4; i++)
     {
-        analog_voltage[i] = ads1.computeVolts(ads1.readADC_SingleEnded(i%4));
+        analog_voltage[i] = i;
+        // analog_voltage[i] = ads1.computeVolts(ads1.readADC_SingleEnded(i%4));
         // ESP_LOGD(TAG,"ads%d = %f", i+1, analog_voltage[i]);
     }
     for(size_t i = 4; i < 8; i++)
     {
-        analog_voltage[i] = ads2.computeVolts(ads2.readADC_SingleEnded(i%4));
+        analog_voltage[i] = i;
+        // analog_voltage[i] = ads2.computeVolts(ads2.readADC_SingleEnded(i%4));
         // ESP_LOGD(TAG,"ads%d = %f", i+1, analog_voltage[i]);
     }
     // ana.Analog_Input_Driver(analog_voltage);
