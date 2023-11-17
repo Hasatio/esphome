d5aae8a63efc05fa904146a38ab2a7a129705dd0
wert95 <wert.95@hotmail.com>
2023-11-18 02:48:41 +0300

    Update ads1115.cpp

--- components/water_quality/ads1115.cpp
+++ components/water_quality/ads1115.cpp
@@ -7,7 +7,7 @@ namespace esphome {
 namespace water_quality {
 
     Analog ana;
-    Water_Quality wq;
+    MyComponent wq;
 
 static const uint8_t ADS1115_REGISTER_CONVERSION = 0x00;
 static const uint8_t ADS1115_REGISTER_CONFIG = 0x01;
