0f0031953cc5b49bc8f5abf2adcf81c0ab587c3c
wert95 <wert.95@hotmail.com>
2023-11-18 16:38:14 +0300

    .

--- components/posture_analyzer/posture_analyzer.cpp
+++ components/posture_analyzer/posture_analyzer.cpp
@@ -2,11 +2,257 @@
 
 namespace esphome {
 namespace posture_analyzer {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Internal Temp
+void MyComponent::internal_temp()
+{
+  uint8_t raw = temprature_sens_read();
+  // ESP_LOGV(TAG, "Raw temperature value: %d", raw);
+  temperature = (raw - 32) / 1.8f;
+  // success = (raw != 128);
+
+  // if (success && std::isfinite(temperature)) {
+  //   this->publish_state(temperature);
+  // } else {
+  //   ESP_LOGD(TAG, "Ignoring invalid temperature (success=%d, value=%.1f)", success, temperature);
+  //   if (!this->has_state()) {
+  //     this->publish_state(NAN);
+  //   }
+  // }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Bluetooth
+void MyComponent::bt_set()
+{
+    SerialBT.begin(btname);
+}
+void MyComponent::bt()
+{
+    data = data + String(x) + "," + String(y) + "," + String(z) + "," + String(voltage) + "," + String(percentage) + "," + String(temperature);
+    // ESP_LOGI("data", "data: %s", data);
+
+    SerialBT.println(data);
+    data = "";
+ 
+    sayac += 1;
     
+    if(SerialBT.available())
+      {
+        mygain = float(SerialBT.read());
+        ESP_LOGD("data", "data: %f", mygain);
+      }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  i2c
+void MyComponent::i2c_set()
+{
+    Wire.begin(SDA,SCL,frq);
+    //Wire1.begin(SDA_1, SCL_1, freq_1);
+    //I2C_1.begin(SDA_1, SCL_1, freq_1);
+    //I2C_2.begin(SDA_2, SCL_2, freq_2);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  ADS1115
+void MyComponent::ads1115_set()
+{  
+    //bool status1 = ads.begin(address1,&I2C);
+    //bool status2 = ads2.begin(address2);
+    //bool status3 = ads3.begin(address3);
+    //bool status4 = ads4.begin(address4);
+
+    if (!ads1.begin(ADS1X15_ADDRESS1,&Wire))
+    {
+      SerialBT.println("Failed to initialize ADS1115_1.");
+      // while (1);
+    }
+    if (!ads2.begin(ADS1X15_ADDRESS2,&Wire))
+    {
+      SerialBT.println("Failed to initialize ADS1115_2.");
+      // while (1);
+    }
+    if (!ads3.begin(ADS1X15_ADDRESS3,&Wire))
+    {
+      SerialBT.println("Failed to initialize ADS1115_3.");
+      // while (1);
+    }
+    if (!ads4.begin(ADS1X15_ADDRESS4,&Wire))
+    {
+      SerialBT.println("Failed to initialize ADS1115_4.");
+      // while (1);
+    }
+
+    // The ADC input range (or gain) can be changed via the following
+    // functions, but be careful never to exceed VDD +0.3V max, or to
+    // exceed the upper and lower limits if you adjust the input range!
+    // Setting these values incorrectly may destroy your ADC!
+    //
+    //                                          ADS1015          ADS1115
+    //                                          -------          -------
+    // GAIN_TWOTHIRDS  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+    // GAIN_ONE        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+    // GAIN_TWO        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+    // GAIN_FOUR       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+    // GAIN_EIGHT      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+    // GAIN_SIXTEEN    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+    ads1.setGain(GAIN_ONE); 
+    ads2.setGain(GAIN_ONE); 
+    ads3.setGain(GAIN_ONE);
+    ads4.setGain(GAIN_ONE);
+    
+    // RATE_ADS1115_8SPS (0x0000)   ///< 8 samples per second
+    // RATE_ADS1115_16SPS (0x0020)  ///< 16 samples per second
+    // RATE_ADS1115_32SPS (0x0040)  ///< 32 samples per second
+    // RATE_ADS1115_64SPS (0x0060)  ///< 64 samples per second
+    // RATE_ADS1115_128SPS (0x0080) ///< 128 samples per second (default)
+    // RATE_ADS1115_250SPS (0x00A0) ///< 250 samples per second
+    // RATE_ADS1115_475SPS (0x00C0) ///< 475 samples per second
+    // RATE_ADS1115_860SPS (0x00E0) ///< 860 samples per second
+    ads1.setDataRate(RATE_ADS1115_860SPS);
+    ads2.setDataRate(RATE_ADS1115_860SPS);
+    ads3.setDataRate(RATE_ADS1115_860SPS);
+    ads4.setDataRate(RATE_ADS1115_860SPS);
+    
+    // ADS1X15_REG_CONFIG_MUX_DIFF_0_1 (0x0000) ///< Differential P = AIN0, N = AIN1 (default)
+    // ADS1X15_REG_CONFIG_MUX_DIFF_0_3 (0x1000) ///< Differential P = AIN0, N = AIN3
+    // ADS1X15_REG_CONFIG_MUX_DIFF_1_3 (0x2000) ///< Differential P = AIN1, N = AIN3
+    // ADS1X15_REG_CONFIG_MUX_DIFF_2_3 (0x3000) ///< Differential P = AIN2, N = AIN3
+    // ADS1X15_REG_CONFIG_MUX_SINGLE_0 (0x4000) ///< Single-ended AIN0
+    // ADS1X15_REG_CONFIG_MUX_SINGLE_1 (0x5000) ///< Single-ended AIN1
+    // ADS1X15_REG_CONFIG_MUX_SINGLE_2 (0x6000) ///< Single-ended AIN2
+    // ADS1X15_REG_CONFIG_MUX_SINGLE_3 (0x7000) ///< Single-ended AIN3
+    // ads.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1);
+}
+void MyComponent::ads1115()
+{
+    for(int i=0;i<4;i++)
+    {
+      adc[i] = ads1.readADC_SingleEnded(i%4);
+      volts[i] = ads1.computeVolts(adc[i]) * mygain;
+      data = data + String(volts[i]) + ",";
+    }
+    for(int i=4;i<8;i++)
+    {
+      adc[i] = ads2.readADC_SingleEnded(i%4);
+      volts[i] = ads2.computeVolts(adc[i]) * mygain;
+      data = data + String(volts[i]) + ",";
+    }
+    for(int i=8;i<12;i++)
+    {
+      adc[i] = ads3.readADC_SingleEnded(i%4);
+      volts[i] = ads3.computeVolts(adc[i]) * mygain;
+      data = data + String(volts[i]) + ",";
+    }
+    for(int i=12;i<16;i++)
+    {
+      adc[i] = ads4.readADC_SingleEnded(i%4);
+      volts[i] = ads4.computeVolts(adc[i]) * mygain;
+      data = data + String(volts[i]) + ",";
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  ADXL345
+void MyComponent::adxl345_set()
+{
+    if (!accel.begin(ADXL345_ADDRESS))
+    {
+      Serial.println("Failed to initialize ADXL345.");
+      // while (1);
+    }
+
+    /* Set the range to whatever is appropriate for your project */
+    //  ADXL345_RANGE_16_G ///< +/- 16g
+    //  ADXL345_RANGE_8_G  ///< +/- 8g
+    //  ADXL345_RANGE_4_G  ///< +/- 4g
+    //  ADXL345_RANGE_2_G  ///< +/- 2g (default value)
+    accel.setRange(ADXL345_RANGE_2_G);
+    
+    //  ADXL345_DATARATE_3200_HZ  ///< 1600Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_1600_HZ  ///<  800Hz Bandwidth    90�A IDD
+    //  ADXL345_DATARATE_800_HZ  ///<  400Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_400_HZ  ///<  200Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_200_HZ  ///<  100Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_100_HZ  ///<   50Hz Bandwidth   140�A IDD
+    //  ADXL345_DATARATE_50_HZ   ///<   25Hz Bandwidth    90�A IDD
+    //  ADXL345_DATARATE_25_HZ   ///< 12.5Hz Bandwidth    60�A IDD
+    //  ADXL345_DATARATE_12_5_HZ ///< 6.25Hz Bandwidth    50�A IDD
+    //  ADXL345_DATARATE_6_25HZ  ///< 3.13Hz Bandwidth    45�A IDD
+    //  ADXL345_DATARATE_3_13_HZ ///< 1.56Hz Bandwidth    40�A IDD
+    //  ADXL345_DATARATE_1_56_HZ ///< 0.78Hz Bandwidth    34�A IDD
+    //  ADXL345_DATARATE_0_78_HZ ///< 0.39Hz Bandwidth    23�A IDD
+    //  ADXL345_DATARATE_0_39_HZ ///< 0.20Hz Bandwidth    23�A IDD
+    //  ADXL345_DATARATE_0_20_HZ ///< 0.10Hz Bandwidth    23�A IDD
+    //  ADXL345_DATARATE_0_10_HZ ///< 0.05Hz Bandwidth    23�A IDD (default value)
+    accel.setDataRate(ADXL345_DATARATE_100_HZ);
+}
+void MyComponent::adxl345()
+{
+    adxlmultiplier = ADXL345_MG2G_MULTIPLIER * SENSORS_GRAVITY_STANDARD;
+    x = accel.getX() * adxlmultiplier;
+    y = accel.getY() * adxlmultiplier;
+    z = accel.getZ() * adxlmultiplier;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  MAX17048
+void MyComponent::max17048_set()
+{
+    if (!maxlipo.begin(&Wire))
+    {
+      Serial.println("Failed to initialize MAX17048.");
+      // while (1);
+    }
+}
+void MyComponent::max17048()
+{
+    voltage = maxlipo.cellVoltage();
+    percentage = maxlipo.cellPercent();
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Sensor
+void MyComponent::sensor()
+{
+    if (this->sample_ != nullptr) this->sample_->publish_state(sayac);
+    if (this->sample_sec_ != nullptr) this->sample_sec_->publish_state(sayac*1000/millis());
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void MyComponent::setup() // ayar fonksiyonu
+{
+    bt_set();
+    i2c_set();
+    ads1115_set();
+    adxl345();
+    max17048_set();
+}    
 void MyComponent::loop() // döngü fonksiyonu
 {
 
 }
+void MyComponent::update()
+{
+    internal_temp();
+    ads1115();
+    adxl345();
+    max17048();
+    bt();
+    sensor();
+}
 
+void MyComponent::bluetooth(String b) // bluetooth fonksiyonu
+{
+    btname = b;
+}
+void MyComponent::gain(float g) // kazanç fonksiyonu
+{
+    mygain = g;
+}
+void MyComponent::sample(sensor::Sensor *sample) // sayaç sensörü fonksiyonu
+{ 
+    sample_ = sample;
+}
+void MyComponent::sample_sec(sensor::Sensor *sample_sec) // sayaç sensörü fonksiyonu
+{ 
+    sample_sec_ = sample_sec;
+}
 } //namespace posture_analyzer
 } //namespace esphome
