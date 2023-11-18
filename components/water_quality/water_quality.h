9e32ab92b41842c1cffad5243ea3c78454039f6c
wert95 <wert.95@hotmail.com>
2023-11-18 17:52:55 +0300

    .

--- components/water_quality/water_quality.h
+++ components/water_quality/water_quality.h
@@ -6,9 +6,10 @@
 #include "esphome.h"
 #include "esphome/core/log.h"
 #include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/i2c/i2c.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
-#include "esphome/components/i2c/i2c.h"
 #include "esphome/core/automation.h"
 #include <array>
 #include <vector>
