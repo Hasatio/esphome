4704165b356135448bc209b5dd82bb24107aa1b4
wert95 <wert.95@hotmail.com>
2023-11-18 15:52:53 +0300

    Update __init__.py

--- components/posture_analyzer/__init__.py
+++ components/posture_analyzer/__init__.py
@@ -45,9 +45,9 @@ async def to_code(config): # fonksiyon tanımlaması
 
     cg.add_library("Wire", None)
     cg.add_library("SPI", None)
+    cg.add_library("Adafruit BusIO",None)
+    cg.add_library("Adafruit Unified Sensor",None)
     cg.add_library("Adafruit ADS1X15",None)
     cg.add_library("Adafruit ADXL345",None)
     cg.add_library("Adafruit MAX1704X",None)
-    cg.add_library("Adafruit BusIO",None)
-    cg.add_library("Adafruit Unified Sensor",None)
     cg.add_library("BluetoothSerial",None)
