1496c37e1eaa45f22d6be2d1c3fcb17ca0b5ac87
wert95 <wert.95@hotmail.com>
2023-11-24 00:40:30 +0300

    Update __init__.py

--- components/water_quality/__init__.py
+++ components/water_quality/__init__.py
@@ -231,7 +231,7 @@ async def to_code(config):
     cg.add(var.ph(ch, t))
 
     # cg.add_library("Wire", None)
-    # cg.add_library("SPI", None)
+    cg.add_library("SPI", None)
     # cg.add_library("Adafruit BusIO",None)
     # cg.add_library("Adafruit ADS1X15", None)
     # cg.add_library("Adafruit MCP23017 Arduino Library", None)
