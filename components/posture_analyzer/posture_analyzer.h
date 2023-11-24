d3016f41798f8e6c6325fe2a8b82974990eb2d65
wert95 <wert.95@hotmail.com>
2023-11-24 06:57:55 +0300

    Update posture_analyzer.h

--- components/posture_analyzer/posture_analyzer.h
+++ components/posture_analyzer/posture_analyzer.h
@@ -78,7 +78,7 @@ void dump_config() override
 {
     ESP_LOGI("data", "Bluetooth is ready to pair\nDevice name: %s",btname);
 }
-void update();
+void update() override;
 
 void bluetooth(String b);
 void gain(float g);
