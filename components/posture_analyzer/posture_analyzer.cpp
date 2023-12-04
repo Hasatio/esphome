ca3c14e88a27e80d7a00010051282753f6caefb2
wert95 <wert.95@hotmail.com>
2023-12-04 09:06:34 +0300

    Update posture_analyzer.cpp

--- components/posture_analyzer/posture_analyzer.cpp
+++ components/posture_analyzer/posture_analyzer.cpp
@@ -67,7 +67,7 @@ void Posture_Analyzer::uuid_set()
   uuid.seed(seed1, seed2);
   uuid.generate();
   uuid.toCharArray();
-  myuuid = uuid;
+  myuuid = uuid.toString();
   ESP_LOGCONFIG(TAG, "uuid: %s", uuid);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
