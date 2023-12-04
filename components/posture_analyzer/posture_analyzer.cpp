10d79bc127e940ab9ac2beb0bb232b2f17660ba0
wert95 <wert.95@hotmail.com>
2023-12-04 09:25:15 +0300

    Update posture_analyzer.cpp

--- components/posture_analyzer/posture_analyzer.cpp
+++ components/posture_analyzer/posture_analyzer.cpp
@@ -67,12 +67,8 @@ void Posture_Analyzer::uuid_set()
   uuid.seed(seed1, seed2);
   uuid.generate();
   ESP_LOGCONFIG(TAG, "uuid: %s", uuid);
-  ESP_LOGCONFIG(TAG, "uuid: %s", uuid.toString());
-  ESP_LOGCONFIG(TAG, "uuid: %s", uuid.c_str());
   uuid.toCharArray();
   ESP_LOGCONFIG(TAG, "uuid: %s", uuid);
-  ESP_LOGCONFIG(TAG, "uuid: %s", uuid.toString());
-  ESP_LOGCONFIG(TAG, "uuid: %s", uuid.c_str());
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Bluetooth
