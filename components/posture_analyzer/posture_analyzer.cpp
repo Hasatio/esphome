35306d04e20a5058dc578a9ff0fc2e96ff118cd5
wert95 <wert.95@hotmail.com>
2023-12-05 13:15:26 +0300

    Update posture_analyzer.cpp

--- components/posture_analyzer/posture_analyzer.cpp
+++ components/posture_analyzer/posture_analyzer.cpp
@@ -57,10 +57,10 @@ void Posture_Analyzer::bt_set()
   pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   // // BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   // pAdvertising->start();
-  // pAdvertising->addServiceUUID(uuid.toCharArray());
-  // pAdvertising->setScanResponse(true);
-  // // // pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-  // // // pAdvertising->setMinPreferred(0x12);
+  pAdvertising->addServiceUUID(uuid.toCharArray());
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
   // // pAdvertising->setScanResponse(false);
   // pAdvertising->setMinPreferred(0x00);
   BLEDevice::startAdvertising();
