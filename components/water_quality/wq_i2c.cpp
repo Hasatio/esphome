da312617b98ef8f3c26bf64703e27da565149b7e
wert95 <wert.95@hotmail.com>
2023-12-09 18:56:42 +0300

    Update wq_i2c.cpp

--- components/water_quality/wq_i2c.cpp
+++ components/water_quality/wq_i2c.cpp
@@ -227,8 +227,8 @@ float WaterQuality::ADS1115_Read(ADS1115Multiplexer multi)
 
 void I2C::test()
 {
-    LOG_I2C_DEVICE(this);
-    if (this->is_failed())
+    LOG_I2C_DEVICE(WaterQuality);
+    if (WaterQuality::is_failed())
         ESP_LOGE(TAG, "var");
     else
         ESP_LOGI(TAG, "yok");
