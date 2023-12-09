3e3ce9f9d640a5e2597eb74ccef58f7d6c7aebc5
wert95 <wert.95@hotmail.com>
2023-12-09 14:29:10 +0300

    Update wq_i2c.cpp

--- components/water_quality/wq_i2c.cpp
+++ components/water_quality/wq_i2c.cpp
@@ -195,7 +195,7 @@ void WaterQuality::ADS1115_Setup(uint8_t address)
 }
 float WaterQuality::ADS1115_Driver()
 {
-  float analog_voltage[];
+  float analog_voltage[8];
   this->set_i2c_address(ADS1X15_ADDRESS1);
   for (size_t i = 0; i < 4; i++)
   {
