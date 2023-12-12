9c0dedc81ae8bbd7de276f867d9311555c5a2b4f
wert95 <wert.95@hotmail.com>
2023-12-12 14:45:46 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -149,18 +149,18 @@ void WaterQuality::loop()
     
         this->MCP23008_update_reg(5, false, MCP23008_GPIO);
         this->MCP23008_update_reg(4, true, MCP23008_GPIO);
-        sleep(100);
+        usleep(100000);
     for (size_t i = 1; i < 4; i++)
     {
         this->MCP23008_update_reg(i + 3, false, MCP23008_GPIO);
         this->MCP23008_update_reg(i + 4, true, MCP23008_GPIO);
-        sleep(100);
+        usleep(100000);
     }
     for (size_t i = 3; i > 1; i--)
     {
         this->MCP23008_update_reg(i + 4, false, MCP23008_GPIO);
         this->MCP23008_update_reg(i + 3, true, MCP23008_GPIO);
-        sleep(100);
+        usleep(100000);
     }
 }
 void WaterQuality::update()
