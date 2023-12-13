28ccdb6304676478f40196f07a693e3e3fed49e2
wert95 <wert.95@hotmail.com>
2023-12-13 16:37:25 +0300

    Update wq_digital.cpp

--- components/water_quality/wq_digital.cpp
+++ components/water_quality/wq_digital.cpp
@@ -7,7 +7,7 @@ namespace water_quality {
 
 void Digital::Digital_Input_Driver(bool inputs[])
 {
-  set_Digital_In(inputs);
+  set_Digital_In(&inputs);
 }
 void Digital::Digital_Output_Driver(bool outputs[])
 {
