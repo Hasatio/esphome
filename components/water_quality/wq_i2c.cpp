2fba0305dc8278e919766c8cdb0db8070c203626
wert95 <wert.95@hotmail.com>
2023-12-14 16:12:04 +0300

    Update wq_i2c.cpp

--- components/water_quality/wq_i2c.cpp
+++ components/water_quality/wq_i2c.cpp
@@ -477,7 +477,43 @@ void WaterQuality::PCA9685_Setup(uint8_t address)
 }
 void WaterQuality::PCA9685_Driver()
 {
+  if (this->min_channel_ == 0xFF || !this->update_)
+    return;
 
+  const uint16_t num_channels = this->max_channel_ - this->min_channel_ + 1;
+  for (uint8_t channel = this->min_channel_; channel <= this->max_channel_; channel++) {
+    uint16_t phase_begin = uint16_t(channel - this->min_channel_) / num_channels * 4096;
+    uint16_t phase_end;
+    uint16_t amount = this->pwm_amounts_[channel];
+    if (amount == 0) {
+      phase_end = 4096;
+    } else if (amount >= 4096) {
+      phase_begin = 4096;
+      phase_end = 0;
+    } else {
+      phase_end = phase_begin + amount;
+      if (phase_end >= 4096)
+        phase_end -= 4096;
+    }
+
+    ESP_LOGVV(TAG, "Channel %02u: amount=%04u phase_begin=%04u phase_end=%04u", channel, amount, phase_begin,
+              phase_end);
+
+    uint8_t data[4];
+    data[0] = phase_begin & 0xFF;
+    data[1] = (phase_begin >> 8) & 0xFF;
+    data[2] = phase_end & 0xFF;
+    data[3] = (phase_end >> 8) & 0xFF;
+
+    uint8_t reg = PCA9685_REGISTER_LED0 + 4 * channel;
+    if (!this->write_bytes(reg, data, 4)) {
+      this->status_set_warning();
+      return;
+    }
+  }
+
+  this->status_clear_warning();
+  this->update_ = false;
 
     // tcaselect(0);
     // for (uint8_t pin=0; pin<16; pin++) 
