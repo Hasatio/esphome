e8712660bc54adba75c3a532d4ad42b55cde550a
wert95 <wert.95@hotmail.com>
2024-01-16 05:45:21 +0300

    Update wq_analog.cpp

--- components/water_quality/wq_analog.cpp
+++ components/water_quality/wq_analog.cpp
@@ -10,14 +10,14 @@ namespace water_quality {
 void Analog::Analog_Input_Driver(float volts[])
 {
     // my.ADS1115_Driver(volts);
-    float WT_Res = (volts[0] * 1000) / (5 - volts[0]) * (get_WT_Res() / 1000); //R2 = (Vout * R1) / (Vin - Vout); Vin = 5V, R1 = 1k
+    float WT_Res = (volts[0] * 1000.0) / (5.0 - volts[0]) * (get_WT_Res() / 1000.0); //R2 = (Vout * R1) / (Vin - Vout); Vin = 5V, R1 = 1k
     set_WT_Val((sqrt((-0.00232 * WT_Res) + 17.59246) - 3.908) / (-0.00116)); //Temp = (√(-0,00232 * R + 17,59246) - 3,908) / -0,00116
 
     set_VP_Val(volts[1] * 6); //Vin = Vout * (R1 + R2) / R2; R1 = 10k, R2 = 2k
     
     float lvl[2], Vmin[2], Vmax[2];
     uint16_t res, volt, *resmin = get_ResMin(), *resmax = get_ResMax();
-    if(ver == 0) {res = 1000; volt = get_VPow_Val();}
+    if(ver == 0) {res = 1000; volt = get_VP_Val();}
     if(ver == 1) {res = 270; volt = 5;}
     Vmin[0] = volt * resmin[0] / (res + resmin[0]); //Vout = Vin * R2 / (R1 + R2); R1 = 10k
     Vmin[1] = volt * resmin[1] / (res + resmin[1]);
