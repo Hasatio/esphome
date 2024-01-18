92e32384c9473dca00b002b3120887079ac61f2a
wert95 <wert.95@hotmail.com>
2024-01-18 14:05:51 +0300

    Update wq_analog.cpp

--- components/water_quality/wq_analog.cpp
+++ components/water_quality/wq_analog.cpp
@@ -11,8 +11,10 @@ void Analog::Analog_Input_Driver(float volts[])
 {
     // my.ADS1115_Driver(volts);
     float WT_Res = (volts[0] * 1000.0) / (5.0 - volts[0]) * (get_WT_Res() / 1000.0); //R2 = (Vout * R1) / (Vin - Vout); Vin = 5V, R1 = 1k
-    set_WT_Val((sqrt((-0.00232 * WT_Res) + 17.59246) - 3.908) / (-0.00116)); //Temp = (√(-0,00232 * R + 17,59246) - 3,908) / -0,00116
-
+    WT_Res = (WT_Res > 3904.8 ? 3904.8 : WT_Res) * (get_WT_Res() / 1000.0); //max temp limit and set model multiplier 
+    float WT = (sqrt((-0.00232 * WT_Res) + 17.59246) - 3.908) / (-0.00116); //Temp = (√(-0,00232 * R + 17,59246) - 3,908) / -0,00116
+    set_WT_Val(WT);
+    
     set_VP_Val(volts[1] * 6); //Vin = Vout * (R1 + R2) / R2; R1 = 10k, R2 = 2k
     
     float lvl[2], Vmin[2], Vmax[2];
