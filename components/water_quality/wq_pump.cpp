75ddeb16330874d24d2ff3bec97b40a009a61dae
wert95 <wert.95@hotmail.com>
2024-01-20 10:37:40 +0300

    Update wq_pump.cpp

--- components/water_quality/wq_pump.cpp
+++ components/water_quality/wq_pump.cpp
@@ -260,7 +260,7 @@ void Pump::Circulation_Controller(uint8_t stat[])
                 stat[i] = 0;
                 break;
             case 1:
-                if (dose[i] > 0)
+                if (circ[i] > 0)
                     if (i % 2 == 0 || (i % 2 == 1 && mode[2 * i - 1] == 0))
                     {
                         stat[i] = circ[i] > calib[i] ? 1 : static_cast<float>(circ[i]) / calib[i];
