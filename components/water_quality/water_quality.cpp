1631d815de095c13cef37bc63a3f97d8c3477772
wert95 <wert.95@hotmail.com>
2024-01-24 15:31:50 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -258,18 +258,20 @@ void WaterQuality::update()
     an.Analog_Input_Driver(a);
 
     dig.Digital_Output_Driver(d);
-    for (size_t i = 0; i < 4; i++)
-        ESP_LOGI(TAG, "d[%d] = %d", i, d[i]);
     MCP23008_Driver(d);
-    for (size_t i = 0; i < 4; i++)
-        ESP_LOGD(TAG, "d[%d] = %d", i, d[i]);
     dig.Digital_Input_Driver(d);
-    for (size_t i = 0; i < 4; i++)
-        ESP_LOGI(TAG, "d[%d] = %d", i, d[i]);
 
-    // pump.Pump_driver(p);
-    // ser.Servo_driver(p);
-    // PCA9685_Driver(p);
+lastChangeTime = millis();
+    
+    pump.Pump_driver(p);
+    ser.Servo_driver(p);
+    PCA9685_Driver(p);
+
+lastTime = millis();
+
+    ESP_LOGI(TAG, "diğer fonk süresi = %d", lastChangeTime - currTime);
+    ESP_LOGI(TAG, "pump süresi = %d", lastTime - lastChangeTime);
+    ESP_LOGI(TAG, "toplam süre = %d", lastTime - currTime);
 
     sensor();
     
