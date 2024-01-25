b956282969d7ebcd92a44551071d81b9400b97f6
wert95 <wert.95@hotmail.com>
2024-01-25 12:27:56 +0300

    .

--- components/water_quality/wq_digital.cpp
+++ components/water_quality/wq_digital.cpp
@@ -5,7 +5,7 @@ namespace water_quality {
 
 void Digital::Digital_Input_Driver(bool inputs[])
 {
-    bool* digital = get_Digital_In();
+    bool* in = get_Digital_In();
 
     for (size_t i = 0; i < 8; i++)
     {
@@ -16,25 +16,25 @@ void Digital::Digital_Input_Driver(bool inputs[])
 
         // if (this->DigIn_FilterCoeff[i] >= 8)
         // {
-            digital[i] = 1;
+            in[i] = 1;
         //     this->DigIn_FilterCoeff[i] = 4;
         // }
         else /*if (this->DigIn_FilterCoeff[i] == 0)
         {*/
-            digital[i] = 0;
+            in[i] = 0;
         //     this->DigIn_FilterCoeff[i] = 4;
         // }
     }
 }
 void Digital::Digital_Output_Driver(bool outputs[])
 {
-    bool* digital = get_Digital_Out();
+    bool* out = get_Digital_Out();
 
     for (size_t i = 0; i < 4; i++)
     {
-        outputs[i] = digital[i];
+        outputs[i] = out[i];
     
-    ESP_LOGI(TAG, "dig = %d", digital[i]);
+    ESP_LOGI(TAG, "out = %d", out[i]);
     }
 }
 
