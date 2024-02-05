e3971a860e6a820a25fe2f05527a2984ecc78b84
wert95 <wert.95@hotmail.com>
2024-02-06 02:12:25 +0300

    Update wq_pump.cpp

--- components/water_quality/wq_pump.cpp
+++ components/water_quality/wq_pump.cpp
@@ -89,7 +89,7 @@ void Pump::Pump_driver(float pwm[])
             ESP_LOGI(TAG, "min = %f", min);
             ESP_LOGI(TAG, "min_ = %f", min_);
 
-    if (min > 0)
+    if (min_ > 0)
     {
         if(min != min_)
         {
