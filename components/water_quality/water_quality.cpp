0edb11e74525f9cbe6982bb23faecf9ba74f49fb
wert95 <wert.95@hotmail.com>
2024-02-05 13:55:15 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -27,7 +27,7 @@ void WaterQuality::setup()
     ADS1115_Setup(ADS1X15_ADDRESS2);
     MCP23008_Setup(MCP23008_ADDRESS);
     PCA9685_Setup(PCA9685_I2C_ADDRESS);	
-    Pupm.Timer_Setup();
+    Pump.Timer_Setup();
 }
 void WaterQuality::dump_config()
 {
