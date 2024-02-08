7b05715adc42efdc4427b362ff5cb55adc42103b
wert95 <wert.95@hotmail.com>
2024-02-08 16:33:27 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -220,10 +220,8 @@ void WaterQuality::pump_type(const std::vector<uint8_t> &ptype)
     // pump.set_Calibration_Mode(start);
     if (start)
         {
-            if (ptype[i] == 1)
-                pump.set_Pump_Dose(calib);
-            if (ptype[i] == 2)
-                pump.set_Pump_Circulation(calib);
+            pump.set_Pump_Dose(calib);
+            pump.set_Pump_Circulation(calib);
             pump.Timer_Setup(6);
         }
         
