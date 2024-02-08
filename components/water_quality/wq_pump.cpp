ed84e23bd67009a20dabcb8d712ca7ed5a8d6a32
wert95 <wert.95@hotmail.com>
2024-02-08 16:01:23 +0300

    Update wq_pump.cpp

--- components/water_quality/wq_pump.cpp
+++ components/water_quality/wq_pump.cpp
@@ -24,7 +24,7 @@ void Pump::Timer_Setup(float period)
     };
     esp_timer_create(&timer_args, &timer);
         
-    ESP_LOGD(TAG, "period = %f", period);
+    ESP_LOGI(TAG, "period = %f", period);
     if (period > 1)
         esp_timer_start_once(timer, static_cast<uint32_t>(period * 1000000));
     else if (period > 0)
@@ -63,7 +63,7 @@ void Pump::Pump_driver(float pwm[])
 
     if (min != min_)
     {
-        ESP_LOGD(TAG, "timer start");
+        ESP_LOGI(TAG, "timer start");
         Timer_Setup(min_);
     }
     else if (min_ == 0)
