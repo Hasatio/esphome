d1ef0f69c4ad2c12ce1446702bce4392fe43a132
wert95 <wert.95@hotmail.com>
2024-02-21 04:25:38 +0300

    Update wq_pump.cpp

--- components/water_quality/wq_pump.cpp
+++ components/water_quality/wq_pump.cpp
@@ -207,10 +207,10 @@ void Pump::Circulation_Controller(float pump[])
             {
                 case 0:
                 ESP_LOGI(TAG,"circ[%d]= %f", i, circ[i]);
-                    if (circ[i] >= 0)
+                    if (circ[i] > 0)
                         if (!(i % 2 == 0 || (i % 2 == 1 && stat[i - 1] != 1)))
                             stat[i] = 0;
-                    else
+                    else if (pump[i] > 0)
                         stat[i] = 2;
                     break;
 
