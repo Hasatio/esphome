de5b4212821d4335d1684df320f4edd4537afe63
wert95 <wert.95@hotmail.com>
2024-04-01 17:16:50 +0300

    .

--- components/veml7700/sensor.py
+++ components/veml7700/sensor.py
@@ -3,10 +3,18 @@ import esphome.config_validation as cv
 from esphome.components import i2c, sensor
 from esphome.const import (
     CONF_ID, 
-    CONF_GAIN, 
+    CONF_ACTUAL_GAIN,
+    CONF_AUTO_MODE,
+    CONF_FULL_SPECTRUM,
+    CONF_GAIN,
+    CONF_GLASS_ATTENUATION_FACTOR,
+    CONF_INFRARED,
+    CONF_INTEGRATION_TIME,
+    CONF_NAME,
     CONF_POWER_SAVE_MODE,
     CONF_WHITE,
     UNIT_LUX,
+    UNIT_MILLISECOND,
 )
 
 from . import (
@@ -18,28 +26,45 @@ from . import (
 CODEOWNERS = ["@hasatio"]
 DEPENDENCIES = ["veml7700"]
 
-CONF_INTETGRATION = "intetgration"
-CONF_PERS = "persisance"
-CONF_LUX = "lux"
-CONF_ALS = "als"
+CONF_ACTUAL_INTEGRATION_TIME = "actual_integration_time"
+CONF_AMBIENT_LIGHT = "ambient_light"
+CONF_AMBIENT_LIGHT_COUNTS = "ambient_light_counts"
+CONF_FULL_SPECTRUM_COUNTS = "full_spectrum_counts"
+CONF_LUX_COMPENSATION = "lux_compensation"
 
 UNIT_RAW = "raw"
+UNIT_COUNTS = "#"
 
 CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(CONF_COMP_ID): cv.use_id(VEML7700),
-            cv.Optional(CONF_LUX): sensor.sensor_schema(
+            cv.Optional(CONF_AMBIENT_LIGHT): sensor.sensor_schema(
                 unit_of_measurement = UNIT_LUX,
                 accuracy_decimals = 1,
             ),
-            cv.Optional(CONF_WHITE): sensor.sensor_schema(
-                unit_of_measurement = UNIT_RAW,
+            cv.Optional(CONF_AMBIENT_LIGHT_COUNTS): sensor.sensor_schema(
+                unit_of_measurement = UNIT_COUNTS,
+                accuracy_decimals = 0,
+            ),
+            cv.Optional(CONF_FULL_SPECTRUM): sensor.sensor_schema(
+                unit_of_measurement = UNIT_LUX,
                 accuracy_decimals = 1,
             ),
-            cv.Optional(CONF_ALS): sensor.sensor_schema(
-                unit_of_measurement = UNIT_RAW,
+            cv.Optional(CONF_FULL_SPECTRUM_COUNTS): sensor.sensor_schema(
+                unit_of_measurement = UNIT_COUNTS,
+                accuracy_decimals = 0,
+            ),
+            cv.Optional(CONF_INFRARED): sensor.sensor_schema(
+                unit_of_measurement = UNIT_LUX,
                 accuracy_decimals = 1,
+            ),
+            cv.Optional(CONF_ACTUAL_GAIN): sensor.sensor_schema(
+                accuracy_decimals = 3,
+            ),
+            cv.Optional(CONF_ACTUAL_INTEGRATION_TIME): sensor.sensor_schema(
+                unit_of_measurement = UNIT_MILLISECOND,
+                accuracy_decimals = 0,
             ),
         }
     )
@@ -51,19 +76,39 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_COMP_ID])
 
-    if CONF_LUX in config:
-        conf = config[CONF_LUX]
+    if CONF_AMBIENT_LIGHT in config:
+        conf = config[CONF_AMBIENT_LIGHT]
         sens = await sensor.new_sensor(conf)
-        cg.add(parent.Lux(sens))
+        cg.add(parent.set_ambient_light_sensor(sens))
         
-    if CONF_WHITE in config:
-        conf = config[CONF_WHITE]
+    if CONF_AMBIENT_LIGHT_COUNTS in config:
+        conf = config[CONF_AMBIENT_LIGHT_COUNTS]
         sens = await sensor.new_sensor(conf)
-        cg.add(parent.White(sens))
+        cg.add(parent.set_ambient_light_counts_sensor(sens))
         
-    if CONF_ALS in config:
-        conf = config[CONF_ALS]
+    if CONF_FULL_SPECTRUM in config:
+        conf = config[CONF_FULL_SPECTRUM]
         sens = await sensor.new_sensor(conf)
-        cg.add(parent.Als(sens))
+        cg.add(parent.set_white_sensor(sens))
+        
+    if CONF_FULL_SPECTRUM_COUNTS in config:
+        conf = config[CONF_FULL_SPECTRUM_COUNTS]
+        sens = await sensor.new_sensor(conf)
+        cg.add(parent.set_white_counts_sensor(sens))
+        
+    if CONF_INFRARED in config:
+        conf = config[CONF_INFRARED]
+        sens = await sensor.new_sensor(conf)
+        cg.add(parent.set_infrared_sensor(sens))
+        
+    if CONF_ACTUAL_GAIN in config:
+        conf = config[CONF_ACTUAL_GAIN]
+        sens = await sensor.new_sensor(conf)
+        cg.add(parent.set_actual_gain_sensor(sens))
+        
+    if CONF_ACTUAL_INTEGRATION_TIME in config:
+        conf = config[CONF_ACTUAL_INTEGRATION_TIME]
+        sens = await sensor.new_sensor(conf)
+        cg.add(parent.set_actual_integration_time_sensor(sens))
         
         
