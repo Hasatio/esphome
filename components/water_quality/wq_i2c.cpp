573aa1ad769524fcf6ddd11ffa6d77019581a305
wert95 <wert.95@hotmail.com>
2024-04-07 03:23:16 +0300

    Update wq_i2c.cpp

--- components/water_quality/wq_i2c.cpp
+++ components/water_quality/wq_i2c.cpp
@@ -589,8 +589,8 @@ void WaterQuality::read_command_result_()
     if (this->is_failed())
         return;
 
-    ESP_LOGI(TAG, "total_volume_dosed_ = %f", this->total_volume_dosed_);
-    ESP_LOGI(TAG, "absolute_total_volume_dosed_ = %f", this->absolute_total_volume_dosed_);
+    // ESP_LOGI(TAG, "total_volume_dosed_ = %f", this->total_volume_dosed_);
+    // ESP_LOGI(TAG, "absolute_total_volume_dosed_ = %f", this->absolute_total_volume_dosed_);
 
     uint8_t response_buffer[21] = {'\0'};
 
