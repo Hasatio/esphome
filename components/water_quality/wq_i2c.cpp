099d979af1497add58bc863beb48f67693b39a22
wert95 <wert.95@hotmail.com>
2024-04-07 03:48:53 +0300

    Update wq_i2c.cpp

--- components/water_quality/wq_i2c.cpp
+++ components/water_quality/wq_i2c.cpp
@@ -1047,14 +1047,14 @@ void WaterQuality::EZOPMP_Driver(float volume[])
     uint8_t command[50] = {0}, len = 20;
     // Örneğin, ilk 4 byte basınç değerini içeriyorsa:
     this->read_bytes_raw(command, len);
+    if (this->command_[0] != command[0])
+        ESP_LOGI(TAG, "command: %s", (char *) command);
     for (size_t i = 0; i < len; i++)
         if (this->command_[i] != command[i])
         {
             this->command_[i] = command[i];
             ESP_LOGI(TAG,"read[%d] = %d", i, command[i]);
         }
-    if (this->command_[0] != command[0])
-        ESP_LOGI(TAG, "command: %s", (char *) command);
         
     for (size_t i = 0; i < 6; i++)
     {
