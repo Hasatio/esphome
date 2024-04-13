34f64a3d89aacecc70fd777bb01941fec2dada68
wert95 <wert.95@hotmail.com>
2024-04-13 19:22:20 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -115,16 +115,16 @@ void WaterQuality::update()
     float a[8], p[16] = {0}, e[6] = {0};
     bool d[4];
 
-    ADS1115_Driver(a);
-    an.Analog_Input_Driver(a);
+    // ADS1115_Driver(a);
+    // an.Analog_Input_Driver(a);
 
-    dig.Digital_Output_Driver(d);
-    MCP23008_Driver(d);
-    dig.Digital_Input_Driver(d);
+    // dig.Digital_Output_Driver(d);
+    // MCP23008_Driver(d);
+    // dig.Digital_Input_Driver(d);
 
-    pump.Generic_Pump_Driver(p);
-    ser.Servo_Driver(p);
-    PCA9685_Driver(p);
+    // pump.Generic_Pump_Driver(p);
+    // ser.Servo_Driver(p);
+    // PCA9685_Driver(p);
 
     pump.Serial_Com_Pump_Driver(e);
     EZOPMP_Driver(e);
