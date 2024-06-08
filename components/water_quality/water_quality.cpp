bd4bcc5315ee7adb83acbd666639ff286add9c54
wert95 <wert.95@hotmail.com>
2024-06-08 09:39:23 +0300

    Update water_quality.cpp

--- components/water_quality/water_quality.cpp
+++ components/water_quality/water_quality.cpp
@@ -67,7 +67,7 @@ void PH_Setup()
     ESP_LOGI(TAG,"Volt1ADDR = %d    eepromVolt1 = %f", Volt1ADDR, eepromVolt1);
     ESP_LOGI(TAG,"PH2ADDR = %d    eepromPH2 = %f", PH2ADDR, eepromPH2);
     ESP_LOGI(TAG,"Volt2ADDR = %d    eepromVolt2 = %f", Volt2ADDR, eepromVolt2);
-    
+
     float PH_Cal[2][2] = {eepromPH1, eepromVolt1, eepromPH2, eepromVolt2};
 
     an.set_PH_Cal(PH_Cal);
@@ -173,8 +173,23 @@ void WaterQuality::dump_config()
     uint16_t *resmin = an.get_ResMin(), *resmax = an.get_ResMax();
     for (uint8_t i = 0; i < sizeof(resmin) / sizeof(resmin[0]); i++)
         ESP_LOGI(TAG, "ResMin[%d] = %d, ResMax[%d] = %d", i, resmin[i], i, resmax[i]);
-    ESP_LOGI(TAG, "EC_ch = %d, EC_type = %d", an.get_EC_Ch(), an.get_EC_Type());
+
+    EEPROM.begin(EEPROM_SIZE);
+    float eepromPH1 = EEPROM_read(PH1ADDR); // Load the value of the pH board from the EEPROM
+    float eepromVolt1 = EEPROM_read(Volt1ADDR); // Load the voltage of the pH board from the EEPROM
+    float eepromPH2 = EEPROM_read(PH2ADDR); // Load the value of the pH board from the EEPROM
+    float eepromVolt2 = EEPROM_read(Volt2ADDR); // Load the voltage of the pH board from the EEPROM
+    
+    EEPROM.commit();
+    EEPROM.end();
+
+    ESP_LOGI(TAG,"PH1ADDR = %d    eepromPH1 = %f", PH1ADDR, eepromPH1);
+    ESP_LOGI(TAG,"Volt1ADDR = %d    eepromVolt1 = %f", Volt1ADDR, eepromVolt1);
+    ESP_LOGI(TAG,"PH2ADDR = %d    eepromPH2 = %f", PH2ADDR, eepromPH2);
+    ESP_LOGI(TAG,"Volt2ADDR = %d    eepromVolt2 = %f", Volt2ADDR, eepromVolt2);
     ESP_LOGI(TAG, "PH_ch = %d, PH_type = %d", an.get_PH_Ch(), an.get_PH_Type());
+    
+    ESP_LOGI(TAG, "EC_ch = %d, EC_type = %d", an.get_EC_Ch(), an.get_EC_Type());
 }
 void WaterQuality::loop()
 {
