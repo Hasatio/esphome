519b2ae5421386f0d3d1d1ee94a33ac087dd576c
wert95 <wert.95@hotmail.com>
2024-08-22 23:18:42 +0300

    Update mcs.cpp

--- components/mcs/mcs.cpp
+++ components/mcs/mcs.cpp
@@ -250,7 +250,7 @@ void MCS::dump_config()
         
     ESP_LOGCONFIG(TAG, "UART:");
     ESP_LOGI(TAG, "  Baud Rate: %d", this->UARTDevice::parent_->get_baud_rate());
-    this->check_uart_settings(115200);
+    this->check_uart_settings(19200);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ODRIVE
